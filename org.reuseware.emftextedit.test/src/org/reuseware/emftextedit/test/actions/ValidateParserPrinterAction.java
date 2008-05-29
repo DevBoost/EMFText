c1e79b6ab6ea0364deb800dadf832cef76006186
Sven Karol <sven.karol@tu-dresden.de>
2008-05-29 14:59:00 +0000

    Initial import.

---
+++ org.reuseware.emftextedit.test/src/org/reuseware/emftextedit/test/actions/ValidateParserPrinterAction.java
@@ -0,0 +1,93 @@
+package org.reuseware.emftextedit.test.actions;
+
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.reuseware.emftextedit.resource.TextResource;
+
+
+public class ValidateParserPrinterAction implements IObjectActionDelegate {
+	
+	private ResourceSet resourceSet;
+	private IFile currentSelection;
+	
+	/**
+	 * Constructor for Action1.
+	 */
+	public ValidateParserPrinterAction() {
+		super();
+		 resourceSet = new ResourceSetImpl();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		String path = currentSelection.getFullPath().toString();
+		TextResource currentTextResource = (TextResource) resourceSet.getResource(URI.createPlatformResourceURI(path, true), true);
+			try {
+				currentTextResource.load(currentSelection.getContents(),null);
+				ByteArrayOutputStream s = new ByteArrayOutputStream();
+				currentTextResource.save(s,null);
+				s.flush();
+				s.close();
+				IFolder parent = (IFolder)currentSelection.getParent();
+				IFile outFile = parent.getFile("test"+currentSelection.getName());
+				if(outFile.exists())
+					outFile.setContents(new ByteArrayInputStream(s.toByteArray()),false,true,null);
+				else
+					outFile.create(new ByteArrayInputStream(s.toByteArray()),false,null);
+			} catch (Exception e) {
+				Shell shell = new Shell();
+				MessageDialog.openInformation(shell,e.getClass().getName(),e.getMessage());
+				e.printStackTrace();
+			}
+
+
+		Shell shell = new Shell();
+		MessageDialog.openInformation(
+			shell,
+			"EMFTextEdit Tests",
+			"New Action was executed.");
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		if(selection instanceof IStructuredSelection){
+			IStructuredSelection structuredSelection = (IStructuredSelection)selection;
+			if(structuredSelection.getFirstElement() instanceof IFile){
+				currentSelection = (IFile)structuredSelection.getFirstElement();
+				
+
+			}
+
+		}
+	
+	}
+
+}
