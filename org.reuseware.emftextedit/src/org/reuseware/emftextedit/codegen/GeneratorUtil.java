92e507b5c4a75c53b4ac1de43338bcb8cbbe3e52
Mirko Seifert <mirko.seifert@devboost.de>
2008-09-22 09:28:17 +0000

    refactored and formatted GeneratorUtil

--- org.reuseware.emftextedit/src/org/reuseware/emftextedit/codegen/GeneratorUtil.java
+++ org.reuseware.emftextedit/src/org/reuseware/emftextedit/codegen/GeneratorUtil.java
@@ -1,7 +1,6 @@
 package org.reuseware.emftextedit.codegen;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.LinkedList;
 
 import org.eclipse.emf.codegen.ecore.genmodel.GenClass;
@@ -12,52 +11,71 @@ import org.reuseware.emftextedit.concretesyntax.Option;
 public class GeneratorUtil {
 
 	/**
-     *  Collects all the subclasses for which concrete syntax is defined.
-     */
-    public static Collection<GenClass> getSubClassesWithCS(GenClass genClass, Collection<Rule> source){
-        Collection<GenClass> subClasses = new LinkedList<GenClass>();
+	 * Collects all the subclasses for which concrete syntax is defined.
+	 */
+	public static Collection<GenClass> getSubClassesWithCS(GenClass genClass,
+			Collection<Rule> source) {
+		Collection<GenClass> subClasses = new LinkedList<GenClass>();
 
-        for (Rule rule : source) {
-     	   	GenClass subClassCand = rule.getMetaclass();
-        		//There seem to be multiple instances of metaclasses when accessed through the genmodel. Therefore, we compare by name
-        		for(EClass superClass : subClassCand.getEcoreClass().getEAllSuperTypes()) {
-        			EClass ecoreClass = genClass.getEcoreClass();
-					if (superClass.getName().equals(ecoreClass.getName()) && 
-        					superClass.getEPackage().getNsURI().equals(ecoreClass.getEPackage().getNsURI())) {
-        				subClasses.add(subClassCand);
-        			}
-        		} 
-        }
-        return subClasses;
-    }
-    
+		for (Rule rule : source) {
+			GenClass subClassCand = rule.getMetaclass();
+			// There seem to be multiple instances of metaclasses when accessed
+			// through the genmodel. Therefore, we compare by name
+			for (EClass superClass : subClassCand.getEcoreClass()
+					.getEAllSuperTypes()) {
+				EClass ecoreClass = genClass.getEcoreClass();
+				if (namesAndPackageURIsAreEqual(superClass, ecoreClass)) {
+					subClasses.add(subClassCand);
+				}
+			}
+		}
+		return subClasses;
+	}
+
 	/**
-     *  Checks whether a subclass with cs does exist.
-     */
-    public static boolean hasSubClassesWithCS(GenClass genClass, Collection<Rule> source){
-         for(Rule rule : source) {
-     	   	GenClass subClassCand = rule.getMetaclass();
-        		for(EClass superClass : subClassCand.getEcoreClass().getEAllSuperTypes()) {
-        			if (superClass.getName().equals(genClass.getEcoreClass().getName()) && 
-        					superClass.getEPackage().getNsURI().equals(genClass.getEcoreClass().getEPackage().getNsURI())) {
-        				return true;
-        			}
-        		} 
-        }
-        return false;
+	 * Checks whether a subclass with concrete syntax does exist.
+	 */
+	public static boolean hasSubClassesWithCS(GenClass genClass,
+			Collection<Rule> source) {
+		for (Rule rule : source) {
+			GenClass subClassCand = rule.getMetaclass();
+			for (EClass superClass : subClassCand.getEcoreClass()
+					.getEAllSuperTypes()) {
+				EClass ecoreClass = genClass.getEcoreClass();
+				if (namesAndPackageURIsAreEqual(superClass, ecoreClass)) {
+					return true;
+				}
+			}
+		}
+		return false;
 
-    }
-    
-    public static Option getOptionByName(String optionName, Collection<Option> options){
-    	if(options!=null&&!options.isEmpty()){
-    		for(Option option:options){
-    			if(optionName.equals(option.getName())){
-    				return option;
-    			}
-    		}
-    	}
-    	return null;
-    }
-    
+	}
 
+	public static Option getOptionByName(String optionName,
+			Collection<Option> options) {
+		if (options != null && !options.isEmpty()) {
+			for (Option option : options) {
+				if (optionName.equals(option.getName())) {
+					return option;
+				}
+			}
+		}
+		return null;
+	}
+
+	private static boolean namesAndPackageURIsAreEqual(EClass classA,
+			EClass classB) {
+		return namesAreEqual(classA, classB) && 
+			packageURIsAreEqual(classA, classB);
+	}
+
+	private static boolean packageURIsAreEqual(EClass classA,
+			EClass classB) {
+		return classA.getEPackage().getNsURI().equals(
+				classB.getEPackage().getNsURI());
+	}
+
+	private static boolean namesAreEqual(EClass classA, EClass classB) {
+		return classA.getName().equals(classB.getName());
+	}
 }
