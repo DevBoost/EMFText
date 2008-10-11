4b5e58ad4f3bfdf204ce3f5ea4fd91d3257064e8
Mirko Seifert <mirko.seifert@devboost.de>
2008-10-11 11:17:34 +0000

    moved two example languages to separate folder

--- org.reuseware.emftextedit.language.simple_template.interpreter/src/org/reuseware/emftextedit/language/simple_template/interpreter/SimpleTemplateInterpreter.java
+++
@@ -1,8 +0,0 @@
-package org.reuseware.emftextedit.language.simple_template.interpreter;
-
-import org.eclipse.emf.ecore.EObject;
-import org.reuseware.emftextedit.language.simple_template.Template;
-
-public interface SimpleTemplateInterpreter {
-	public EObject interprete(Template template, EObject inputModel);
-}
