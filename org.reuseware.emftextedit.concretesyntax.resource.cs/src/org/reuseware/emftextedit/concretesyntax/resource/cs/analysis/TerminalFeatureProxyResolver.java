d5cfd51ac9bcef7eb0a8b6588fdd03887b48a845
Mirko Seifert <mirko.seifert@devboost.de>
2008-10-15 20:03:32 +0000

    improved error message for features that cannot be resolved

--- org.reuseware.emftextedit.concretesyntax.resource.cs/src/org/reuseware/emftextedit/concretesyntax/resource/cs/analysis/TerminalFeatureProxyResolver.java
+++ org.reuseware.emftextedit.concretesyntax.resource.cs/src/org/reuseware/emftextedit/concretesyntax/resource/cs/analysis/TerminalFeatureProxyResolver.java
@@ -12,10 +12,50 @@ import org.reuseware.emftextedit.resource.impl.ProxyResolverImpl;
 import org.reuseware.emftextedit.concretesyntax.Placeholder;
 
 public class TerminalFeatureProxyResolver extends ProxyResolverImpl {
+	
+	private class Pair<S,T> {
+		public final S left;
+		public final T right;
+
+		public Pair(S s, T t) {
+			this.left = s;
+			this.right = t;
+		}
+	}
 		
 	@Override
 	protected EObject doResolve(InternalEObject proxy, EObject container,
 			EReference reference, TextResource resource) {
+		return doResolveFeature(proxy, container).left;
+	}
+
+	private Pair<EObject, Boolean> doResolveFeature(InternalEObject proxy, EObject container) {
+		Rule rule = getContainingRule(container);
+		
+		if (rule.getMetaclass().eIsProxy()) {
+			return new Pair<EObject, Boolean>(null, false);
+		}
+		
+		boolean foundFeatureWithCorrectName = false;
+		for (GenFeature feature : rule.getMetaclass().getAllGenFeatures()) {
+			URI proxyURI = proxy.eProxyURI();
+			String featureName = feature.getName();
+			if (featureName == null) {
+				continue;
+			}
+			if (featureName.equals(proxyURI.fragment())) {
+				foundFeatureWithCorrectName = true;
+				boolean isContainment = feature.getEcoreFeature() instanceof EReference && ((EReference)feature.getEcoreFeature()).isContainment();
+				
+				if(container instanceof Placeholder && !isContainment
+					|| container instanceof Containment && isContainment)
+					return new Pair<EObject, Boolean>(feature, true);
+			}
+		}
+		return new Pair<EObject, Boolean>(null, foundFeatureWithCorrectName);
+	}
+
+	private Rule getContainingRule(EObject container) {
 		Rule rule = null;
 		EObject o = container;
 		do {
@@ -25,41 +65,27 @@ public class TerminalFeatureProxyResolver extends ProxyResolverImpl {
 			else {
 				o = o.eContainer();
 			}
-		} while(rule == null);
-		
-		if (rule.getMetaclass().eIsProxy()) return null;
-		
-		
-		for(GenFeature feature : rule.getMetaclass().getAllGenFeatures()) {
-			URI proxyURI = proxy.eProxyURI();
-			String featureName = feature.getName();
-			if (featureName == null) {
-				continue;
-			}
-			if (featureName.equals(proxyURI.fragment())) {
-				boolean isContainment = feature.getEcoreFeature() instanceof EReference && ((EReference)feature.getEcoreFeature()).isContainment();
-				
-				if(container instanceof Placeholder && !isContainment
-					|| container instanceof Containment && isContainment)
-					return feature;
-			}
-		}
-		
-		produceResolveErrorMessage(proxy,container,reference,resource);
-		
-		return null;
+		} while (rule == null);
+		return rule;
 	}
 
 	@Override
 	protected String produceResolveErrorMessage(InternalEObject proxy,
 			EObject container, EReference reference, TextResource resource) {
-		String msg = "Feature \"" + proxy.eProxyURI().fragment() + "\" does not exist or has wrong containment type.";
-		return msg;
+		// TODO it is somewhat stupid to resolve the proxy here again, but
+		// the result of doResolve() is not known when this method is
+		// called. in order to give a more meaningful error message the 
+		// proxy must be resolved again.
+		boolean foundFeatureWithCorrectName = doResolveFeature(proxy, container).right;
+		if (foundFeatureWithCorrectName) {
+			return "Feature \"" + proxy.eProxyURI().fragment() + "\" does exist, but has wrong containment type.";
+		} else {
+			return "Feature \"" + proxy.eProxyURI().fragment() + "\" does not exist.";
+		}
 	}
 	
 	public String deResolve(EObject element, EObject container,EReference reference){
 		GenFeature feature = (GenFeature)element;
 		return feature.getName();
 	}
-	
 }
