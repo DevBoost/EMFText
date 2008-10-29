c3229f5821946146a77b7b13cc45f8924399356d
Florian Heidenreich <florian.heidenreich@devboost.de>
2008-10-29 15:54:10 +0000

    Changed to load concrete syntax via org.reuseware.emftextedit.concretesyntax extension point. Added writing of snapshots in case of fail.

--- org.reuseware.emftextedit.test/src/org/reuseware/emftextedit/test/resource/DeterministicLoadTest.java
+++ org.reuseware.emftextedit.test/src/org/reuseware/emftextedit/test/resource/DeterministicLoadTest.java
@@ -2,9 +2,19 @@ package org.reuseware.emftextedit.test.resource;
 
 import static org.junit.Assert.fail;
 
+import java.util.Collections;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.compare.diff.metamodel.DiffFactory;
 import org.eclipse.emf.compare.diff.metamodel.DiffGroup;
 import org.eclipse.emf.compare.diff.metamodel.DiffModel;
+import org.eclipse.emf.compare.diff.metamodel.ModelInputSnapshot;
 import org.eclipse.emf.compare.diff.service.DiffService;
 import org.eclipse.emf.compare.match.metamodel.MatchModel;
 import org.eclipse.emf.compare.match.service.MatchService;
@@ -12,39 +22,76 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.junit.Before;
 import org.junit.Test;
+import org.reuseware.emftextedit.EMFTextEditPlugin;
+import org.reuseware.emftextedit.MetamodelManager;
 import org.reuseware.emftextedit.concretesyntax.resource.cs.CsResourceFactoryImpl;
 import org.reuseware.emftextedit.resource.impl.TextResourceImpl;
+import org.reuseware.emftextedit.ui.actions.GenPackageInRegistryFinder;
 
 public class DeterministicLoadTest {
 
-	private static final URI csInputUri = URI.createFileURI("input/java.cs");
 	private static final String CS_FILE_EXTENSION = "cs";
+	private static final URI csUri = EMFTextEditPlugin
+			.getURIToConcreteSyntaxLocationMap().get(
+					"http://www.reuseware.org/emftextedit/concretesyntax%%cs");
+
+	private static final String OUTPUT_FOLDER = "snapshots";
+	private static final String OUTPUT_PROJECT = "DeterministicLoadTest";
+
+	@Before
+	public void setUp() throws Exception {
+		createOutputProject(OUTPUT_PROJECT, OUTPUT_FOLDER);
+
+		MetamodelManager.INSTANCE
+				.addGenPackageFinder(new GenPackageInRegistryFinder());
+	}
 
 	@Test
 	public void testDeterministicLoad() throws Exception {
-		final EObject modelObject1 = loadModel(csInputUri);
-		final EObject modelObject2 = loadModel(csInputUri);
-		
-		compareModels(modelObject1, modelObject2);
-	}
-	
-	@Test
-	public void testDeterministicLoadSave() throws Exception {
-		fail("Not yet implemented");
+		final EObject modelObject1 = loadModel(csUri);
+		final EObject modelObject2 = loadModel(csUri);
+
+		compareModels(modelObject1, modelObject2, "testDeterministicLoad");
 	}
 
-	private void compareModels(final EObject modelLeft, final EObject modelRight)
-			throws Exception {
+	private void compareModels(final EObject modelLeft,
+			final EObject modelRight, String testName) throws Exception {
 		final MatchModel inputMatch = MatchService.doMatch(modelLeft,
 				modelRight, null);
 		final DiffModel inputDiff = DiffService.doDiff(inputMatch);
 
 		if (((DiffGroup) inputDiff.getOwnedElements().get(0)).getSubchanges() != 0) {
+			final ModelInputSnapshot snapshot = DiffFactory.eINSTANCE
+					.createModelInputSnapshot();
+			snapshot.setDiff(inputDiff);
+			snapshot.setMatch(inputMatch);
+			saveModel(snapshot, testName);
 			fail("Diff failed");
 		}
 	}
-	
+
+	private void createOutputProject(String projectName, String folderName)
+			throws Exception {
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceRoot root = workspace.getRoot();
+		IProject project = root.getProject(projectName);
+		if (!project.exists()) {
+			project.create(null);
+		}
+
+		if (!project.isOpen()) {
+			project.open(null);
+		}
+
+		IFolder inputFolder = project.getFolder(folderName);
+		if (!inputFolder.exists()) {
+			inputFolder.create(IResource.NONE, true, null);
+		}
+
+	}
+
 	private EObject loadModel(final URI modelFilUri) {
 		return loadModelResource(modelFilUri).getContents().get(0);
 	}
@@ -53,6 +100,14 @@ public class DeterministicLoadTest {
 		// every resource is loaded in its own ResourceSet to prevent caching effects
 		ResourceSet resourceSet = new ResourceSetImpl();
 
+		registerFactory(resourceSet);
+		resourceSet.getLoadOptions().put(TextResourceImpl.OPTION_NO_VALIDATE,
+				true);
+
+		return resourceSet.getResource(modelFileUri, true);
+	}
+
+	private void registerFactory(final ResourceSet resourceSet) {
 		final Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
 		final Object resourceFactory = reg.getExtensionToFactoryMap().get(
 				CS_FILE_EXTENSION);
@@ -63,9 +118,17 @@ public class DeterministicLoadTest {
 			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
 					.put(CS_FILE_EXTENSION, new CsResourceFactoryImpl());
 		}
+	}
 
-		resourceSet.getLoadOptions().put(TextResourceImpl.OPTION_NO_VALIDATE, true);
+	private void saveModel(EObject root, String snapshotName) throws Exception {
+		final URI modelURI = URI.createPlatformResourceURI(OUTPUT_PROJECT + "/"
+				+ OUTPUT_FOLDER + "/" + snapshotName, true);
+		final ResourceSet resourceSet = new ResourceSetImpl();
 
-		return resourceSet.getResource(modelFileUri, true);
+		registerFactory(resourceSet);
+
+		final Resource newModelResource = resourceSet.createResource(modelURI);
+		newModelResource.getContents().add(root);
+		newModelResource.save(Collections.EMPTY_MAP);
 	}
 }
