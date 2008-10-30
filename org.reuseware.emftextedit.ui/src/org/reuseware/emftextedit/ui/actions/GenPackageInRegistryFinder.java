d7eac87f3e7ac10df1235058bdab427274a9863f
Florian Heidenreich <florian.heidenreich@devboost.de>
2008-10-30 16:13:30 +0000

    Added GenPackageInRegistryFinderResult private inner class.

--- org.reuseware.emftextedit.ui/src/org/reuseware/emftextedit/ui/actions/GenPackageInRegistryFinder.java
+++ org.reuseware.emftextedit.ui/src/org/reuseware/emftextedit/ui/actions/GenPackageInRegistryFinder.java
@@ -8,11 +8,29 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.reuseware.emftextedit.GenPackageFinder;
+import org.reuseware.emftextedit.IGenPackageFinderResult;
 import org.reuseware.emftextedit.resource.TextResource;
 
 public class GenPackageInRegistryFinder implements GenPackageFinder {
+	
+	private class GenPackageInRegistryFinderResult implements IGenPackageFinderResult {
 
-	public GenPackage findGenPackage(String nsURI, TextResource resource) {
+		private GenPackage genPackage;
+		
+		public GenPackageInRegistryFinderResult(GenPackage genPackage) {
+			this.genPackage = genPackage;
+		}
+		
+		public boolean hasChanged() {
+			return false;
+		}
+
+		public GenPackage getResult() {
+			return genPackage;
+		}
+	}
+
+	public IGenPackageFinderResult findGenPackage(String nsURI, TextResource resource) {
 		final ResourceSet rs = new ResourceSetImpl();
 		//search all registered generator models
         for(URI genModelURI : EcorePlugin.getEPackageNsURIToGenModelLocationMap().values()) {
@@ -21,7 +39,7 @@ public class GenPackageInRegistryFinder implements GenPackageFinder {
             	GenModel genModel = (GenModel) genModelResource.getContents().get(0);
             	for(GenPackage genPackage : genModel.getGenPackages()) {
             		if (nsURI.equals(genPackage.getNSURI())) {
-            			return genPackage;
+            			return new GenPackageInRegistryFinderResult(genPackage);
             		}
             	}
         	} catch (Exception e ) {
