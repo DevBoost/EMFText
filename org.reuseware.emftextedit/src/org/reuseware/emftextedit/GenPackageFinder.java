9ad3ed57a291c32c3ac44d838e8107dd37e8cec2
Florian Heidenreich <florian.heidenreich@devboost.de>
2008-10-30 16:12:08 +0000

    Added IGenPackageFinderResult

--- org.reuseware.emftextedit/src/org/reuseware/emftextedit/GenPackageFinder.java
+++ org.reuseware.emftextedit/src/org/reuseware/emftextedit/GenPackageFinder.java
@@ -1,10 +1,9 @@
 package org.reuseware.emftextedit;
 
-import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
 import org.reuseware.emftextedit.resource.TextResource;
 
 public interface GenPackageFinder {
 
-	public GenPackage findGenPackage(String nsURI, TextResource resource);
+	public IGenPackageFinderResult findGenPackage(String nsURI, TextResource resource);
 
 }
---
+++ org.reuseware.emftextedit/src/org/reuseware/emftextedit/IGenPackageFinderResult.java
@@ -0,0 +1,26 @@
+package org.reuseware.emftextedit;
+
+import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
+
+/**
+ * Implementations of this interface are instantiated by implementations of
+ * {@link GenPackageFinder}. The main purpose of this interface is to
+ * encapsulate a {@link GenPackage} and make sure that it is possible to
+ * determine if it needs to be updated.
+ */
+public interface IGenPackageFinderResult {
+
+	/**
+	 * @return The encapsulated {@link GenPackage}.
+	 */
+	GenPackage getResult();
+
+	/**
+	 * This method is used to determine if the encapsulated {@link GenPackage}
+	 * has changed and needs to be updated.
+	 * 
+	 * @return <code>true</code> iff the encapsulated {@link GenPackage} has
+	 *         changed, <code>false</code> otherwise.
+	 */
+	boolean hasChanged();
+}
