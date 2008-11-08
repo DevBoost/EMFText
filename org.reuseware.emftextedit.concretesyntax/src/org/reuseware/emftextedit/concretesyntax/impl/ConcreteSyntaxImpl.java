cce69ba06b78aded41ca00fbe83527cc5b45643f
Mirko Seifert <mirko.seifert@devboost.de>
2008-11-08 19:41:30 +0000

    removed unused import

--- org.reuseware.emftextedit.concretesyntax/src/org/reuseware/emftextedit/concretesyntax/impl/ConcreteSyntaxImpl.java
+++ org.reuseware.emftextedit.concretesyntax/src/org/reuseware/emftextedit/concretesyntax/impl/ConcreteSyntaxImpl.java
@@ -10,27 +10,20 @@ import java.util.Collection;
 
 import org.eclipse.emf.codegen.ecore.genmodel.GenClass;
 import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
-
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
-
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
-
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.InternalEObject;
-
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
-
 import org.eclipse.emf.ecore.util.EObjectContainmentEList;
 import org.eclipse.emf.ecore.util.EObjectContainmentWithInverseEList;
 import org.eclipse.emf.ecore.util.EObjectEList;
-import org.eclipse.emf.ecore.util.EObjectResolvingEList;
 import org.eclipse.emf.ecore.util.EcoreEList;
 import org.eclipse.emf.ecore.util.InternalEList;
-
 import org.reuseware.emftextedit.concretesyntax.ConcreteSyntax;
 import org.reuseware.emftextedit.concretesyntax.ConcretesyntaxPackage;
 import org.reuseware.emftextedit.concretesyntax.Import;
