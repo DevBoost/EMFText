64c0ff5cee5c52a16cd5cc44791d07a5c67fcd8e
Mirko Seifert <mirko.seifert@devboost.de>
2008-12-11 11:59:16 +0000

    added TODO item

--- org.emftext.runtime/src/org/emftext/runtime/resource/impl/IdentifierMappingImpl.java
+++ org.emftext.runtime/src/org/emftext/runtime/resource/impl/IdentifierMappingImpl.java
@@ -5,7 +5,8 @@ import org.emftext.runtime.resource.IdentifierMapping;
 public class IdentifierMappingImpl extends ReferenceMappingImpl implements IdentifierMapping {
 	
 	private String newIdentifier;
-	
+
+	// TODO mseifert: change the second parameter to be a URI
 	public IdentifierMappingImpl(String identifier, String newIdentifier, String warning) {
 		super(identifier, warning);
 		this.newIdentifier = newIdentifier;
