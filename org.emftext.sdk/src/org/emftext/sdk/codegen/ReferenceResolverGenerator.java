e45a56abe90c96abbb324790ae55136e5cac6a92
Mirko Seifert <mirko.seifert@devboost.de>
2008-12-16 23:17:40 +0000

    added TODO item

--- org.emftext.sdk/src/org/emftext/sdk/codegen/ReferenceResolverGenerator.java
+++ org.emftext.sdk/src/org/emftext/sdk/codegen/ReferenceResolverGenerator.java
@@ -10,6 +10,8 @@ import org.emftext.runtime.resource.impl.ReferenceResolverImpl;
 
 /**
  * Generates basic stubs for reference resolvers.
+ * 
+ * TODO mseifert: change suffix for classes from IReferenceResolver to ReferenceResolver 
  */
 public class ReferenceResolverGenerator extends BaseGenerator {
 	
