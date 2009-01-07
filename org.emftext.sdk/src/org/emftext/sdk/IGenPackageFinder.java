bcf31c7074950004c48601c250396b6b241b2680
Mirko Seifert <mirko.seifert@devboost.de>
2009-01-07 16:47:55 +0000

    added some documentation

--- org.emftext.sdk/src/org/emftext/sdk/IGenPackageFinder.java
+++ org.emftext.sdk/src/org/emftext/sdk/IGenPackageFinder.java
@@ -2,9 +2,20 @@ package org.emftext.sdk;
 
 import org.eclipse.emf.ecore.resource.Resource;
 
-
+/**
+ * An IGenPackageFinder can be used to look up the EMF generator package ofr
+ * a given URI.
+ */
 public interface IGenPackageFinder {
 
+	/**
+	 * Searches for all generator packages that match the given nsURI.
+	 * 
+	 * @param nsURI the URI of the generator package we search for
+	 * @param resource the resource that contains a link to the generator package
+	 *                 (needed if generator packages that reside in the same folder 
+	 *                 shall be preferred)
+	 * @return
+	 */
 	public IGenPackageFinderResult findGenPackage(String nsURI, Resource resource);
-
 }
