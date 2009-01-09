0903a825b449761cc1232ccc8010b6505eeec7de
Mirko Seifert <mirko.seifert@devboost.de>
2009-01-09 14:01:42 +0000

    added initial implementation for another CS reprint problem check

--- org.emftext.sdk/src/org/emftext/sdk/SDKOptionProvider.java
+++ org.emftext.sdk/src/org/emftext/sdk/SDKOptionProvider.java
@@ -1,26 +1,34 @@
 package org.emftext.sdk;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import org.eclipse.emf.codegen.ecore.genmodel.GenFeature;
 import org.eclipse.emf.ecore.EObject;
 import org.emftext.runtime.IOptionProvider;
 import org.emftext.runtime.IOptions;
 import org.emftext.runtime.IResourcePostProcessor;
 import org.emftext.runtime.IResourcePostProcessorProvider;
 import org.emftext.runtime.resource.ITextResource;
+import org.emftext.sdk.concretesyntax.Choice;
 import org.emftext.sdk.concretesyntax.CompoundDefinition;
 import org.emftext.sdk.concretesyntax.CsString;
 import org.emftext.sdk.concretesyntax.Definition;
 import org.emftext.sdk.concretesyntax.QUESTIONMARK;
+import org.emftext.sdk.concretesyntax.Rule;
 import org.emftext.sdk.concretesyntax.STAR;
 import org.emftext.sdk.concretesyntax.Sequence;
+import org.emftext.sdk.concretesyntax.Terminal;
 
 public class SDKOptionProvider implements IOptionProvider {
 
 	private static final String OPTIONAL_KEYWORD_WARNING = 
 		"The keyword might be used stand alone and will not be reprinted in such case: ";
+	private static final String MULTIPLE_FEATURE_WARNING = 
+		"The follwing feature is used twice in rule. Reprinting may fail for: ";
 	
 	public Map<?, ?> getOptions() {
 		Map<String, Object> options = new HashMap<String, Object>();
@@ -30,6 +38,7 @@ public class SDKOptionProvider implements IOptionProvider {
 				return new IResourcePostProcessor() {
 					public void process(ITextResource resource) {
 						checkForOptionalKeywords(resource);
+						//checkForDuplicateReferences(resource);
 					}
 				};
 			}
@@ -38,7 +47,7 @@ public class SDKOptionProvider implements IOptionProvider {
 		return options;
 	}
 
-	protected void checkForOptionalKeywords(ITextResource resource) {
+	private void checkForOptionalKeywords(ITextResource resource) {
 		for(Iterator<EObject> i = resource.getAllContents(); i.hasNext(); ) {
 			EObject next = i.next();
 			if (next instanceof CompoundDefinition) {
@@ -72,5 +81,57 @@ public class SDKOptionProvider implements IOptionProvider {
 				}
 			}
 		}
+	}
+
+	private void checkForDuplicateReferences(ITextResource resource) {
+		for(Iterator<EObject> i = resource.getAllContents(); i.hasNext(); ) {
+			EObject next = i.next();
+			if (next instanceof Rule) {
+				List<GenFeature> features = collectReferencedFeatures((Rule) next);
+				for (GenFeature feature : features) {
+					if (isContainedMoreThanOnce(feature, features)) {
+						resource.addWarning(
+								MULTIPLE_FEATURE_WARNING + feature.getEcoreFeature().getName(),
+								next);
+					}
+				}
+			}
+		}
+	}
+
+	private boolean isContainedMoreThanOnce(GenFeature feature,
+			List<GenFeature> features) {
+		int count = 0;
+		for (GenFeature next : features) {
+			if (next.equals(feature)) {
+				count++;
+			}
+		}
+		return count > 1;
+	}
+
+	private List<GenFeature> collectReferencedFeatures(Rule rule) {
+		return collectReferencedFeatures(rule.getDefinition());
+	}
+	
+	private List<GenFeature> collectReferencedFeatures(Choice choice) {
+		List<GenFeature> result = new ArrayList<GenFeature>();
+		List<Sequence> choices = choice.getOptions();
+		for (Sequence sequence : choices) {
+			List<Definition> definitions = sequence.getParts();
+			for (Definition definition : definitions) {
+				// TODO incorporate cardinality of the definition, because
+				// not all combinations of cardinalities cause reprint problems
+				if (definition instanceof Terminal) {
+					Terminal terminal = (Terminal) definition;
+					result.add(terminal.getFeature());
+				} else if (definition instanceof CompoundDefinition) {
+					CompoundDefinition compound = (CompoundDefinition) definition;
+					Choice subChoice = compound.getDefinitions();
+					result.addAll(collectReferencedFeatures(subChoice));
+				}
+			}
+		}
+		return result;
 	}
 }
