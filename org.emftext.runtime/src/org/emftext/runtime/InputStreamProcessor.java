ac84877bd22ac36b00aabcc2b4256799851f24b5
Mirko Seifert <mirko.seifert@devboost.de>
2009-01-20 22:20:04 +0000

    added method getOutputEncoding() to InputStreamProcessor to allow the ANTLRStream to know how characters are encoded adjusted code generation to support character encoding

--- org.emftext.runtime/src/org/emftext/runtime/InputStreamProcessor.java
+++ org.emftext.runtime/src/org/emftext/runtime/InputStreamProcessor.java
@@ -2,5 +2,17 @@ package org.emftext.runtime;
 
 import java.io.InputStream;
 
+/**
+ * A InputStreamProcessor can be used like a normal InputStream,
+ * but provides information about the encoding that is used to
+ * represent characters as bytes.
+ */
 public abstract class InputStreamProcessor extends InputStream {
+	
+	/**
+	 * Returns the encoding of the characters that can be read
+	 * from this InputStreamProcessor. This encoding is passed
+	 * to subsequent streams (e.g., the ANTLRInputStream).
+	 */
+	public abstract String getOutputEncoding();
 }
