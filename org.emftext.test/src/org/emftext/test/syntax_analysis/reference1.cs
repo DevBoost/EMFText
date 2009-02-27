480b4b990c3336539dba2233e7189099d8342a52
Mirko Seifert <mirko.seifert@devboost.de>
2009-02-27 19:33:40 +0000

    fixed typo in comment

--- org.emftext.test/src/org/emftext/test/syntax_analysis/reference1.cs
+++ org.emftext.test/src/org/emftext/test/syntax_analysis/reference1.cs
@@ -3,7 +3,7 @@ FOR <http://www.emftext.org/test/references> <references.genmodel>
 START Root
 
 RULES {
-	// here we should get an errors, because the type of reference 
+	// here we should get an error, because the type of reference 
 	// 'toAbstract' is AbstractClassA, which has no concrete sub classes.
 	Root ::= "ROOT" (toAbstract[])*;
 }
