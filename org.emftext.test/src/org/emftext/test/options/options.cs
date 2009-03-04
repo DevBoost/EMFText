11755d45edad6e9442d19cfcfeb73bbbdf085e16
Mirko Seifert <mirko.seifert@devboost.de>
2009-03-04 17:04:13 +0000

    added missing quotes

--- org.emftext.test/src/org/emftext/test/options/options.cs
+++ org.emftext.test/src/org/emftext/test/options/options.cs
@@ -3,11 +3,11 @@ FOR <http://www.reuseware.org/test/options>
 START Dummy
 
 OPTIONS {
-	tokenspace = 1;
-	autofixSimpleLeftrecursion = false;
-	forceEOF = true; 
-	standardTextTokenName = TEXT;
-	usePredefinedTokens = false;
+	tokenspace = "1";
+	autofixSimpleLeftrecursion = "false";
+	forceEOF = "true"; 
+	standardTextTokenName = "TEXT";
+	usePredefinedTokens = "false";
 }
 
 TOKENS {
