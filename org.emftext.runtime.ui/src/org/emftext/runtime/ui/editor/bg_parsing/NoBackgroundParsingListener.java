6facb0c233b6de58c7daf090fa131d45e9eb4894
Mirko Seifert <mirko.seifert@devboost.de>
2009-03-04 14:12:15 +0000

    added comment removed TODO

--- org.emftext.runtime.ui/src/org/emftext/runtime/ui/editor/bg_parsing/NoBackgroundParsingListener.java
+++ org.emftext.runtime.ui/src/org/emftext/runtime/ui/editor/bg_parsing/NoBackgroundParsingListener.java
@@ -2,11 +2,12 @@ package org.emftext.runtime.ui.editor.bg_parsing;
 
 import org.eclipse.emf.ecore.resource.Resource;
 
+/**
+ * A background parsing listener that does nothing.
+ */
 public class NoBackgroundParsingListener implements IBackgroundParsingListener {
 
 	public void parsingCompleted(Resource resource) {
-		// TODO Auto-generated method stub
-
+		// do nothing
 	}
-
 }
