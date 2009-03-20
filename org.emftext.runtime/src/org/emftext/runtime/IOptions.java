27f1cedf301ab3f8356bf2d65c24b1b9d809163e
Sven Karol <sven.karol@tu-dresden.de>
2009-03-20 15:17:07 +0000

    Optionname and additional Exception type to handle different contenttypes when parsing text resources

--- org.emftext.runtime/src/org/emftext/runtime/IOptions.java
+++ org.emftext.runtime/src/org/emftext/runtime/IOptions.java
@@ -36,4 +36,9 @@ public interface IOptions {
 	 * The key for the option to provide a resource post-processor.
 	 */
 	public String RESOURCE_POSTPROCESSOR_PROVIDER = "RESOURCE_POSTPROCESSOR_PROVIDER";
+	
+	/**
+	 * The key for the option specify an expected content type in text resources and text parsers.
+	 */
+	public final String RESOURCE_CONTENT_TYPE = "RESOURCE_CONTENT_TYPE"; 
 }
---
+++ org.emftext.runtime/src/org/emftext/runtime/resource/impl/UnexpectedContentTypeException.java
@@ -0,0 +1,23 @@
+package org.emftext.runtime.resource.impl;
+
+import org.antlr.runtime.RecognitionException;
+
+/**
+ * Exception class to represent invalid content types for parser instances.
+ * 
+ * @see org.emftext.runtime.IOptions.RESOURCE_CONTENT_TYPE
+ */
+public class UnexpectedContentTypeException extends RecognitionException{
+	
+	private static final long serialVersionUID = 4791359811519433999L;
+	
+	private Object contentType = null;
+	
+	public UnexpectedContentTypeException(Object contentType){
+		this.contentType = contentType;
+	}
+	
+	public Object getContentType(){
+		return contentType;
+	}
+}
