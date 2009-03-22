073e7dcdae54d514293df0e918340117a0c647b8
Mirko Seifert <mirko.seifert@devboost.de>
2009-03-22 19:42:58 +0000

    added example token styles

--- org.emftext.sdk.concretesyntax/metamodel/concretesyntax.cs
+++ org.emftext.sdk.concretesyntax/metamodel/concretesyntax.cs
@@ -12,8 +12,14 @@ TOKENS {
 	DEFINE COMMENTS $'//'(~('\n'|'\r'))*$;
 	DEFINE QUALIFIED_NAME $('A'..'Z'|'a'..'z'|'_')('A'..'Z'|'a'..'z'|'_'|'-'|'0'..'9')*('.'('A'..'Z'|'a'..'z'|'_'|'-'|'0'..'9')+)*$;
 	DEFINE NUMBER $('0'..'9')+$;
+	DEFINE HEXNUMBER $'#'('0'..'9'|'A'..'F'|'a'..'f')+$;
 	DEFINE WHITESPACE $(' '|'\t'|'\f')$;
 	DEFINE LINEBREAK $('\r\n'|'\r'|'\n')$;
+}
+
+TOKENSTYLES {
+	NUMBER COLOR #00D0FF;
+	HEXNUMBER COLOR #00D0FF;
 }
 
 RULES {
@@ -26,6 +32,7 @@ RULES {
     (!0 !0 "IMPORTS" "{" ( !2 imports)* !0 "}")? 
     (!0 !0 "OPTIONS" "{" (!2 options ";" )*  !0 "}")? 
     (!0 !0 "TOKENS" "{" ( !2 tokens ";")* !0 "}")? 
+    (!0 !0 "TOKENSTYLES" "{" ( !2 tokenStyles)* !0 "}")? 
     !0 !0 "RULES" "{" ( !2 rules+) !0"}"
     ;
 
@@ -51,7 +58,7 @@ RULES {
   
   CompoundDefinition ::= "(" definitions ")" cardinality?;
 
-  WhiteSpaces  ::= "#" amount[NUMBER] #1;
+  WhiteSpaces  ::= amount[HEXNUMBER] #1;
   LineBreak    ::= "!" tab[NUMBER] #1;
   
   NormalToken     ::= "DEFINE" #1 name[] regex['$','$'] ("COLLECT" "IN" attributeName[])?;
@@ -62,4 +69,6 @@ RULES {
   QUESTIONMARK ::= "?";
   
   Abstract ::= "ABSTRACT";
+  
+  TokenStyle ::= tokenName[] "COLOR" rgb[HEXNUMBER] ("," fontStyles[])* ";";
 }
