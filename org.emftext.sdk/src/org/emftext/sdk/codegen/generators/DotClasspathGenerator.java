233a77442cfe62d551ded9fa2ce90deda4222844
Mirko Seifert <mirko.seifert@devboost.de>
2009-04-13 18:16:20 +0000

    adjusted classpath generator to new SOURCE_FOLDER option

--- org.emftext.sdk/src/org/emftext/sdk/codegen/generators/DotClasspathGenerator.java
+++ org.emftext.sdk/src/org/emftext/sdk/codegen/generators/DotClasspathGenerator.java
@@ -3,22 +3,36 @@ package org.emftext.sdk.codegen.generators;
 import java.io.PrintWriter;
 
 import org.emftext.sdk.codegen.GenerationContext;
+import org.emftext.sdk.codegen.OptionManager;
 import org.emftext.sdk.codegen.composites.StringComposite;
 import org.emftext.sdk.codegen.composites.XMLComposite;
+import org.emftext.sdk.concretesyntax.OptionTypes;
 
 public class DotClasspathGenerator extends BaseGenerator {
 
+	private GenerationContext context;
+
 	public DotClasspathGenerator(GenerationContext context) {
-		super("", ".project");
+		super("", ".classpath");
+		this.context = context;
 	}
 
 	@Override
 	public boolean generate(PrintWriter out) {
+		
+		String sourceOptionValue = OptionManager.INSTANCE.getStringOptionValue(context.getConcreteSyntax(), OptionTypes.SOURCE_FOLDER);
+		String sourceFolder;
+		if (sourceOptionValue == null) {
+			sourceFolder = "src";
+		} else {
+			sourceFolder = sourceOptionValue;
+		}
+		
 		StringComposite sc = new XMLComposite();
 		
 		sc.add("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 		sc.add("<classpath>");
-		sc.add("<classpathentry kind=\"src\" path=\"src\"/>");
+		sc.add("<classpathentry kind=\"src\" path=\"" + sourceFolder + "\"/>");
 		sc.add("<classpathentry kind=\"con\" path=\"org.eclipse.jdt.launching.JRE_CONTAINER\"/>");
 		sc.add("<classpathentry kind=\"con\" path=\"org.eclipse.pde.core.requiredPlugins\"/>");
 		sc.add("<classpathentry kind=\"output\" path=\"bin\"/>");
@@ -27,30 +41,4 @@ public class DotClasspathGenerator extends BaseGenerator {
 		out.write(sc.toString());
 		return true;
 	}
-
-	/*
-	private void setClasspath(GenerationContext context, SubMonitor progress)
-	throws JavaModelException {
-	
-	UIGenerationContext uiContext = (UIGenerationContext) context;
-
-	IWorkspace workspace = ResourcesPlugin.getWorkspace();
-	IWorkspaceRoot root = workspace.getRoot();
-	
-	IPath srcLocation= Path.fromOSString(context.getSourceFolder().getAbsolutePath());
-	IFile[] srcFolders = root.findFilesForLocation(srcLocation);
-	IFile srcFolder = srcFolders[0];
-	
-	IPath outLocation= Path.fromOSString(context.getOutputFolder().getAbsolutePath());
-	IFile[] outFolders = root.findFilesForLocation(outLocation);
-	IFile outFolder = outFolders[0];
-	
-	uiContext.getJavaProject().setRawClasspath(new IClasspathEntry[] {
-			JavaCore.newSourceEntry(srcFolder.getFullPath()),
-			JavaCore.newContainerEntry(new Path(JavaRuntime.JRE_CONTAINER)),
-			JavaCore.newContainerEntry(new Path(
-					"org.eclipse.pde.core.requiredPlugins")) }, outFolder
-			.getFullPath(), progress.newChild(TICKS_SET_CLASSPATH));
-	}
-	*/
 }
