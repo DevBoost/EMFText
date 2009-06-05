03db942b5b62701d9cd86f3cb5d23e4884a08cfd
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-05 11:42:57 +0000

    extended test case for code completion

--- org.emftext.test.code_completion.test/src/org/emftext/test/code_completion/test/ParseToCursorTest.java
+++ org.emftext.test.code_completion.test/src/org/emftext/test/code_completion/test/ParseToCursorTest.java
@@ -7,8 +7,13 @@ import java.io.FileNotFoundException;
 import junit.framework.TestCase;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.emftext.runtime.resource.ITextParser;
 import org.emftext.runtime.resource.ITextResource;
+import org.emftext.runtime.resource.impl.AbstractEMFTextParser;
+import org.emftext.runtime.resource.impl.ReachedCursorIndexException;
+import org.emftext.runtime.resource.impl.AbstractEMFTextParser.IExpectedElement;
+import org.emftext.test.code_completion.Code_completionPackage;
 import org.emftext.test.code_completion.resource.cct.CctParser;
 import org.emftext.test.code_completion.resource.cct.CctResourceFactory;
 
@@ -17,10 +22,20 @@ public class ParseToCursorTest extends TestCase {
 	private static final String INPUT_DIR = "input";
 
 	public void testParseToCursor() {
-		parseToCursor("ClassA.cct", 2);
+		parseToCursor("KeywordClassExpected.cct", 8, "class");
+		parseToCursor("EnumVisibilityExpected.cct", 2, Code_completionPackage.eINSTANCE.getModifiable_Visibility());
+		parseToCursor("BracketExpected.cct", 15, "{");
 	}
 	
-	private void parseToCursor(String filename, int cursorIndex) {
+	private void parseToCursor(String filename, int cursorIndex, String expectedCsString) {
+		parseToCursor(filename, cursorIndex, new AbstractEMFTextParser.ExpectedCsString(expectedCsString));
+	}
+	
+	private void parseToCursor(String filename, int cursorIndex, EStructuralFeature feature) {
+		parseToCursor(filename, cursorIndex, new AbstractEMFTextParser.ExpectedStructuralFeature(feature));
+	}
+	
+	private void parseToCursor(String filename, int cursorIndex, IExpectedElement expectedCompletionElement) {
 		FileInputStream inputStream = null;
 		try {
 			inputStream = new FileInputStream(INPUT_DIR + File.separator + filename);
@@ -31,6 +46,12 @@ public class ParseToCursorTest extends TestCase {
 		ITextParser parser = factory.createInstance(inputStream, null);
 		ITextResource resource = (ITextResource) new CctResourceFactory().createResource(URI.createFileURI("dummyResource.cct"));
 		parser.setResource(resource);
-		parser.parseToIndex(cursorIndex);
+		try {
+			parser.parseToIndex(cursorIndex);
+			fail("Expected ReachedCursorIndexException.");
+		} catch (ReachedCursorIndexException pcie) {
+			final IExpectedElement actualElement = pcie.getExpectedElement();
+			assertEquals(expectedCompletionElement, actualElement);
+		}
 	}
 }
