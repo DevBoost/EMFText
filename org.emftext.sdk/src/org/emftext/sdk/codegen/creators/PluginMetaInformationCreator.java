a0cdbde6fcf7629f3dbe8b2c7a702b67b5142266
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-07 16:10:08 +0000

    added creator and generator for plug-in meta information class (bug 777: Provide Metainformation on EMFText languages)

---
+++ org.emftext.sdk/src/org/emftext/sdk/codegen/creators/PluginMetaInformationCreator.java
@@ -0,0 +1,34 @@
+package org.emftext.sdk.codegen.creators;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.emftext.sdk.codegen.GenerationContext;
+import org.emftext.sdk.codegen.IGenerator;
+import org.emftext.sdk.codegen.generators.PluginMetaInformationGenerator;
+import org.emftext.sdk.concretesyntax.OptionTypes;
+
+public class PluginMetaInformationCreator extends AbstractArtifactCreator {
+
+	private static final String PLUG_IN_META_INFORMATION = "plug-in meta information class";
+
+	public PluginMetaInformationCreator() {
+		super(PLUG_IN_META_INFORMATION);
+	}
+
+	@Override
+	public Collection<IArtifact> getArtifactsToCreate(GenerationContext context) {
+		IGenerator generator = new PluginMetaInformationGenerator(context);
+        File file = context.getMetaInformationClassFile();
+		return createArtifact(
+	    		context,
+	    		generator,
+	    		file,
+	    		"Exception while generating " + PLUG_IN_META_INFORMATION + "."
+	    );
+	}
+
+	public OptionTypes getOverrideOption() {
+		return OptionTypes.OVERRIDE_PLUGIN_META_INFORMATION_CLASS;
+	}
+}
---
+++ org.emftext.sdk/src/org/emftext/sdk/codegen/generators/PluginMetaInformationGenerator.java
@@ -0,0 +1,66 @@
+package org.emftext.sdk.codegen.generators;
+
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import org.eclipse.emf.codegen.ecore.genmodel.GenClass;
+import org.eclipse.emf.ecore.EClass;
+import org.emftext.runtime.resource.ITextParser;
+import org.emftext.runtime.resource.ITextResourcePluginMetaInformation;
+import org.emftext.sdk.codegen.GenerationContext;
+import org.emftext.sdk.codegen.composites.JavaComposite;
+import org.emftext.sdk.codegen.composites.StringComposite;
+import org.emftext.sdk.codegen.util.GenClassUtil;
+import org.emftext.sdk.codegen.util.GeneratorUtil;
+import org.emftext.sdk.concretesyntax.ConcreteSyntax;
+
+public class PluginMetaInformationGenerator extends BaseGenerator {
+
+	private final static GeneratorUtil generatorUtil = new GeneratorUtil();
+	private final static GenClassUtil genClassUtil = new GenClassUtil();
+	
+	private String parserClassName;
+	private ConcreteSyntax syntax;
+
+	public PluginMetaInformationGenerator(GenerationContext context) {
+		super(context.getPackageName(), context.getMetaInformationClassName());
+		this.parserClassName = context.getQualifiedParserClassName();
+		this.syntax = context.getConcreteSyntax();
+	}
+	
+	@Override
+	public boolean generate(PrintWriter out) {
+		StringComposite sc = new JavaComposite();
+		
+        sc.add("package " + getResourcePackageName() + ";");
+		sc.addLineBreak();
+        
+        sc.add("public class " + getResourceClassName()+ " implements " + ITextResourcePluginMetaInformation.class.getName() + " {");
+        sc.addLineBreak();
+		addCreateParserMethod(sc);
+        sc.addLineBreak();
+		addGetClassesWithSyntaxMethod(sc);
+		sc.add("}");
+    	
+		out.print(sc.toString());
+    	return true;	
+	}
+
+	private void addCreateParserMethod(StringComposite sc) {
+		sc.add("public " + ITextParser.class.getName() + " createParser(" + InputStream.class.getName() + " inputStream, " + String.class.getName() + " encoding) {");
+		sc.add("return new " + parserClassName + "().createInstance(inputStream, encoding);");
+		sc.add("}");
+	}
+
+	private void addGetClassesWithSyntaxMethod(StringComposite sc) {
+		Collection<GenClass> classesWithSyntax = generatorUtil.getClassesWithSyntax(syntax);
+		sc.add("public " + EClass.class.getName() + "[] getClassesWithSyntax() {");
+		sc.add("return new " + EClass.class.getName() + "[] {");
+		for (GenClass classWithSyntax : classesWithSyntax) {
+			sc.add(genClassUtil.getAccessor(classWithSyntax) + ",");
+		}
+		sc.add("};");
+		sc.add("}");
+	}
+}
