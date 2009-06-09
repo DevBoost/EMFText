5ef1ab77fc0a4a8e43f37d0755beddb4968ede0d
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-09 14:50:11 +0000

    tagged modified generated code

--- org.emftext.sdk.concretesyntax.edit/src-gen/org/emftext/sdk/concretesyntax/provider/AnnotationItemProvider.java
+++ org.emftext.sdk.concretesyntax.edit/src-gen/org/emftext/sdk/concretesyntax/provider/AnnotationItemProvider.java
@@ -140,7 +140,7 @@ public class AnnotationItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
