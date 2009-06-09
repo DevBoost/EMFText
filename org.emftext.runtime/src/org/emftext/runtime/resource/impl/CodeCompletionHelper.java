2c1024845eb576d17424e1c410fbf7c546b92e04
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-09 20:34:03 +0000

    move most of the code for code completion to the runtime plug-in to test it without the UI dependencies

---
+++ org.emftext.runtime/src/org/emftext/runtime/resource/impl/CodeCompletionHelper.java
@@ -0,0 +1,115 @@
+package org.emftext.runtime.resource.impl;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EEnum;
+import org.eclipse.emf.ecore.EEnumLiteral;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.emftext.runtime.resource.ITextParser;
+import org.emftext.runtime.resource.ITextResource;
+import org.emftext.runtime.resource.ITextResourcePluginMetaInformation;
+import org.emftext.runtime.util.EClassUtil;
+import org.emftext.runtime.util.StringUtil;
+
+public class CodeCompletionHelper {
+
+	private final static EClassUtil eClassUtil = new EClassUtil();
+
+	public List<String> computeCompletionProposals(ITextResource resource, String document, int offset) {
+
+		ITextResourcePluginMetaInformation metaInformation = resource.getMetaInformation();
+		String content = document;
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(content.getBytes());
+		ITextParser parser = metaInformation.createParser(inputStream, null);
+		final IExpectedElement expectedElement = parser.parseToIndex(offset, null);
+		if (expectedElement == null) {
+			return Collections.emptyList();
+		}
+		System.out.println("computeCompletionProposals() " + expectedElement + " for offset " + offset);
+		return deriveProposals(expectedElement, content, metaInformation, offset);
+	}
+
+	private List<String> deriveProposals(
+			IExpectedElement expectedElement, String content, ITextResourcePluginMetaInformation metaInformation, int offset) {
+		if (expectedElement instanceof ExpectedCsString) {
+			ExpectedCsString csString = (ExpectedCsString) expectedElement;
+			return deriveProposal(csString, content, offset);
+		}
+		if (expectedElement instanceof ExpectedStructuralFeature) {
+			ExpectedStructuralFeature expectedFeature = (ExpectedStructuralFeature) expectedElement;
+			EStructuralFeature feature = expectedFeature.getFeature();
+			EClassifier featureType = feature.getEType();
+			if (featureType instanceof EEnum) {
+				EEnum enumType = (EEnum) featureType;
+				return deriveProposals(enumType, content, offset);
+			}
+			if (feature instanceof EReference) {
+				EReference refernce = (EReference) feature;
+				if (featureType instanceof EClass) {
+					if (refernce.isContainment()) {
+						EClass classType = (EClass) featureType;
+						return deriveProposals(classType, metaInformation, content, offset);
+					} else {
+						// TODO handle NC references
+					}
+				}
+			} else {
+				// TODO handle EAttributes
+			}
+		}
+		return null;
+	}
+
+	private List<String> deriveProposals(
+			EClass type,
+			ITextResourcePluginMetaInformation metaInformation, 
+			String content, int offset) {
+		List<String> allProposals = new ArrayList<String>();
+		// find all subtypes and call parseToIndex
+		EClass[] availableClasses = metaInformation.getClassesWithSyntax();
+		Collection<EClass> allSubClasses = eClassUtil.getSubClasses(type, availableClasses);
+		for (EClass subClass : allSubClasses) {
+			ITextParser parser = metaInformation.createParser(new ByteArrayInputStream(new byte[0]), null);
+			final IExpectedElement expectedElement = parser.parseToIndex(0, subClass);
+			if (expectedElement == null) {
+				continue;
+			}
+			System.out.println("computeCompletionProposals() " + expectedElement + " for offset " + offset);
+			List<String> proposals = deriveProposals(expectedElement, content, metaInformation, offset);
+			for (String nextProposal : proposals) {
+				allProposals.add(nextProposal);
+			}
+		}
+		return allProposals;
+	}
+
+	private List<String> deriveProposals(EEnum enumType, String content, int offset) {
+		List<EEnumLiteral> enumLiterals = enumType.getELiterals();
+		List<String> result = new ArrayList<String>(enumLiterals.size());
+		for (EEnumLiteral literal : enumLiterals) {
+			String proposal = literal.getLiteral();
+			String contentBefore = content.substring(0, offset);
+			String insertString = StringUtil.getMissingTail(contentBefore, proposal);
+			result.add(insertString);
+		}
+		return result;
+	}
+
+	private List<String> deriveProposal(ExpectedCsString csString,
+			String content, int offset) {
+		String proposal = csString.getValue();
+		String contentBefore = content.substring(0, offset);
+		String insertString = StringUtil.getMissingTail(contentBefore, proposal);
+		
+		List<String> result = new ArrayList<String>(1);
+		result.add(insertString);
+		return result;
+	}
+}
