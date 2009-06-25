2143155df05ed2201fc94f7cd2218c145d433759
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-25 15:05:25 +0000

    fixed warning message

--- org.emftext.sdk/src/org/emftext/sdk/syntax_analysis/ReferencesAnalyser.java
+++ org.emftext.sdk/src/org/emftext/sdk/syntax_analysis/ReferencesAnalyser.java
@@ -93,7 +93,7 @@ public class ReferencesAnalyser extends AbstractPostProcessor {
 				resource, 
 				syntax, 
 				unchangeableReferences,
-				"Reference %s is not changeable."
+				"Reference '%2$s' is not changeable."
 		);
 	}
 
