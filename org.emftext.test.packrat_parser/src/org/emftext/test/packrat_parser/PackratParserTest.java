d18ad4c4008c719f8f68d373c65ef61d3fc99064
Mirko Seifert <mirko.seifert@devboost.de>
2009-07-14 09:15:08 +0000

    added more tests and build script

--- org.emftext.test.packrat_parser/src/org/emftext/test/packrat_parser/PackratParserTest.java
+++ org.emftext.test.packrat_parser/src/org/emftext/test/packrat_parser/PackratParserTest.java
@@ -38,17 +38,23 @@ public class PackratParserTest extends TestCase {
 	public static class GrammarFeatureParseTest extends TestCase {
 		
 		private String content;
+		private boolean expectedResult;
 
-		public GrammarFeatureParseTest(String content) {
+		public GrammarFeatureParseTest(String content, boolean expectedResult) {
 			super("Parse " + content.replace("\n", "").replace("\r", ""));
 			this.content = content;
+			this.expectedResult = expectedResult;
 		}
 		
 		public void runTest() {
 			ByteArrayInputStream in = new ByteArrayInputStream(content.getBytes());
 			ITextParser parser = new Grammar_featuresPackratParser().createInstance(in, null);
 			EObject root = parser.parse();
-			assertNotNull("Parsing should be successful.", root);
+			if (expectedResult) {
+				assertNotNull("Parsing should be successful.", root);
+			} else {
+				assertNull("Parsing should be fail.", root);
+			}
 		}
 	}
 	
@@ -59,6 +65,70 @@ public class PackratParserTest extends TestCase {
 		suite.addTest(new CctParseTest("public class A {private A x;}"));
 		suite.addTest(new CctParseTest("public class A {private A x;private A y;}"));
 		suite.addTest(new CctParseTest("public class A {private void method() {}}"));
+		
+		suite.addTest(new GrammarFeatureParseTest("co", true));
+		suite.addTest(new GrammarFeatureParseTest("co a b", true));
+
+		// this should not be parsable
+		suite.addTest(new GrammarFeatureParseTest("co a b a b", false));
+		suite.addTest(new GrammarFeatureParseTest("co a c", false));
+		suite.addTest(new GrammarFeatureParseTest("some thing stupid", false));
+		
+		suite.addTest(new GrammarFeatureParseTest("cs", true));
+		suite.addTest(new GrammarFeatureParseTest("cs a b", true));
+		suite.addTest(new GrammarFeatureParseTest("cs a b a b", true));
+		suite.addTest(new GrammarFeatureParseTest("cs a b a b ab", true));
+
+		suite.addTest(new GrammarFeatureParseTest("cp", false));
+		suite.addTest(new GrammarFeatureParseTest("cp a b", true));
+		suite.addTest(new GrammarFeatureParseTest("cp a b a b", true));
+		suite.addTest(new GrammarFeatureParseTest("cp a b a b ab", true));
+
+		suite.addTest(new GrammarFeatureParseTest("cs", true));
+		suite.addTest(new GrammarFeatureParseTest("cs a b", true));
+		suite.addTest(new GrammarFeatureParseTest("cs a b a b", true));
+		suite.addTest(new GrammarFeatureParseTest("cs a b a b ab", true));
+		
+		suite.addTest(new GrammarFeatureParseTest("mc", false));
+		suite.addTest(new GrammarFeatureParseTest("mc x", true));
+		suite.addTest(new GrammarFeatureParseTest("mc x x", false));
+
+		suite.addTest(new GrammarFeatureParseTest("oc", true));
+		suite.addTest(new GrammarFeatureParseTest("oc x", true));
+		suite.addTest(new GrammarFeatureParseTest("oc x x", false));
+
+		suite.addTest(new GrammarFeatureParseTest("pc", false));
+		suite.addTest(new GrammarFeatureParseTest("pc x", true));
+		suite.addTest(new GrammarFeatureParseTest("pc x x", true));
+
+		suite.addTest(new GrammarFeatureParseTest("sc", true));
+		suite.addTest(new GrammarFeatureParseTest("sc x", true));
+		suite.addTest(new GrammarFeatureParseTest("sc x x", true));
+
+		suite.addTest(new GrammarFeatureParseTest("mnc", false));
+		suite.addTest(new GrammarFeatureParseTest("mnc xyz", true));
+		suite.addTest(new GrammarFeatureParseTest("mnc xyz xyz", false));
+
+		suite.addTest(new GrammarFeatureParseTest("onc", true));
+		suite.addTest(new GrammarFeatureParseTest("onc xyz", true));
+		suite.addTest(new GrammarFeatureParseTest("onc xyz xyz", false));
+
+		suite.addTest(new GrammarFeatureParseTest("pnc", false));
+		suite.addTest(new GrammarFeatureParseTest("pnc xyz", true));
+		suite.addTest(new GrammarFeatureParseTest("pnc xyz xyz", true));
+
+		suite.addTest(new GrammarFeatureParseTest("snc", true));
+		suite.addTest(new GrammarFeatureParseTest("snc xyz", true));
+		suite.addTest(new GrammarFeatureParseTest("snc xyz xyz", true));
+		
+		suite.addTest(new GrammarFeatureParseTest("alternativeA", true));
+		suite.addTest(new GrammarFeatureParseTest("alternativeB", true));
+		suite.addTest(new GrammarFeatureParseTest("alternativeA alternativeB", true));
+		
+		suite.addTest(new GrammarFeatureParseTest("concreteA", true));
+		suite.addTest(new GrammarFeatureParseTest("concreteB", true));
+		suite.addTest(new GrammarFeatureParseTest("concreteA concreteB", true));
+		
 		return suite;
 	}
 }
