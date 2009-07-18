d7969d167151740398f5fa5c252fbd1e3ddefe7d
Mirko Seifert <mirko.seifert@devboost.de>
2009-07-18 21:58:19 +0000

    added documentation

--- org.emftext.runtime/src/org/emftext/runtime/resource/ITextLexer.java
+++ org.emftext.runtime/src/org/emftext/runtime/resource/ITextLexer.java
@@ -1,7 +1,19 @@
 package org.emftext.runtime.resource;
 
+/**
+ * A common interface for scanners to be used by EMFText.
+ * A scanner is initialized with a text and delivers a 
+ * sequence of tokens.
+ */
 public interface ITextLexer {
 
+	/**
+	 * Sets the text that must be scanned.
+	 */
 	public void setText(String text);
+	
+	/**
+	 * Returns the next token found in the text.
+	 */
 	public ITextToken getNextToken();
 }
