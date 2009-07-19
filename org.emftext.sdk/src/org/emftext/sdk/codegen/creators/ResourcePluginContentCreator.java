16ecea738560176c1446e3aac62e5440110bec8e
Mirko Seifert <mirko.seifert@devboost.de>
2009-07-19 13:23:56 +0000

    removed unused import

--- org.emftext.sdk/src/org/emftext/sdk/codegen/creators/ResourcePluginContentCreator.java
+++ org.emftext.sdk/src/org/emftext/sdk/codegen/creators/ResourcePluginContentCreator.java
@@ -20,7 +20,6 @@
  ******************************************************************************/
 package org.emftext.sdk.codegen.creators;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
