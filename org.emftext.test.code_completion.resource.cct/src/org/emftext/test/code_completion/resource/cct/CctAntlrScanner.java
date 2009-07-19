ca24b035dede3246e2d75b4c9e820d34634b61e7
Mirko Seifert <mirko.seifert@devboost.de>
2009-07-19 20:25:20 +0000

    changed ignore list

---
+++ org.emftext.test.code_completion.resource.cct/src/org/emftext/test/code_completion/resource/cct/CctAntlrScanner.java
@@ -0,0 +1,65 @@
+package org.emftext.test.code_completion.resource.cct;
+
+public class CctAntlrScanner extends org.emftext.runtime.resource.impl.AbstractEMFTextScanner {
+	
+	private java.lang.String[] tokenNames;
+	private org.antlr.runtime.Lexer antlrLexer;
+	
+	public CctAntlrScanner(org.emftext.runtime.resource.ITextResourcePluginMetaInformation metaInformation, org.antlr.runtime.Lexer antlrLexer) {
+		this.tokenNames = metaInformation.getTokenNames();
+		this.antlrLexer = antlrLexer;
+	}
+	
+	public org.emftext.runtime.resource.ITextToken getNextToken() {
+		final org.antlr.runtime.Token current = antlrLexer.nextToken();
+		org.emftext.runtime.resource.ITextToken result = new org.emftext.runtime.resource.ITextToken() {
+			
+			public java.lang.String getName() {
+				return getTokenName(tokenNames, current.getType());
+			}
+			
+			public int getOffset() {
+				return ((org.antlr.runtime.CommonToken) current).getStartIndex();
+			}
+			
+			public int getLength() {
+				return ((org.antlr.runtime.CommonToken) current).getStopIndex() - ((org.antlr.runtime.CommonToken) current).getStartIndex() + 1;
+			}
+			
+			public boolean canBeUsedForSyntaxHighlighting() {
+				int tokenType = current.getType();
+				if (tokenType == org.antlr.runtime.Token.EOF) {
+					return false;
+				}
+				if (tokenType == org.antlr.runtime.Token.UP) {
+					return false;
+				}
+				if (tokenType == org.antlr.runtime.Token.DOWN) {
+					return false;
+				}
+				if (tokenType == org.antlr.runtime.Token.EOR_TOKEN_TYPE) {
+					return false;
+				}
+				if (tokenType == org.antlr.runtime.Token.INVALID_TOKEN_TYPE) {
+					return false;
+				}
+				return true;
+			}
+		};
+		return result;
+	}
+	
+	public void setText(java.lang.String text) {
+		antlrLexer.setCharStream(new org.antlr.runtime.ANTLRStringStream(text));
+	}
+	public java.lang.String getTokenName(java.lang.String[] tokenNames, int index) {
+		if (tokenNames == null) {
+			return null;
+		}
+		java.lang.String tokenName = tokenNames[index];
+		if (tokenName != null && tokenName.startsWith("'")) {
+			tokenName = tokenName.substring(1, tokenName.length() - 1).trim();
+		}
+		return tokenName;
+	}
+}
