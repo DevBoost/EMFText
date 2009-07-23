749e734d2886181f8cee590d74a2f702319d80b8
Mirko Seifert <mirko.seifert@devboost.de>
2009-07-23 21:02:43 +0000

    aligned interface ExtensionConstants with EMFText coding style

--- org.emftext.runtime.ui/src/org/emftext/runtime/ui/extensions/ExtensionConstants.java
+++ org.emftext.runtime.ui/src/org/emftext/runtime/ui/extensions/ExtensionConstants.java
@@ -1,16 +1,14 @@
 package org.emftext.runtime.ui.extensions;
 
-//TODO mseifert: align this class with the EMFText coding style
+/**
+ * An enumeration of all position categories.
+ * 
+ * TODO hoang-kim can we use an enumeration instead?
+ */
 public interface ExtensionConstants {
-
 	public static final String POSITION_CATEGORY_BRACKET = "bracket";
-
 	public static final String POSITION_CATEGORY_DEF = "definition";
-
 	public static final String POSITION_CATEGORY_USE = "use";
-
 	public static final String POSITION_CATEGORY_HYPERLINK = "hyperlink";
-
 	public static final String POSITION_CATEGORY_DESTINATION = "destination";
-
 }
