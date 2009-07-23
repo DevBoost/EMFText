ced910fb73bfee888e899c229d8ca04267752b06
Mirko Seifert <mirko.seifert@devboost.de>
2009-07-23 20:31:39 +0000

    aligned class Occurence with EMFText coding style

--- org.emftext.runtime.ui/src/org/emftext/runtime/ui/editor/EMFTextEditor.java
+++ org.emftext.runtime.ui/src/org/emftext/runtime/ui/editor/EMFTextEditor.java
@@ -22,7 +22,6 @@ package org.emftext.runtime.ui.editor;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ResourceBundle;
--- org.emftext.runtime.ui/src/org/emftext/runtime/ui/extensions/Occurrence.java
+++ org.emftext.runtime.ui/src/org/emftext/runtime/ui/extensions/Occurrence.java
@@ -20,157 +20,172 @@ import org.emftext.runtime.ui.ColorManager;
 import org.emftext.runtime.ui.EMFTextRuntimeUIPlugin;
 import org.emftext.runtime.ui.EMFTextTokenScanner;
 
-//TODO mseifert: align this class with the EMFText coding style
+//TODO hoang-kim add documentation
 public class Occurrence {
 
-	private EMFTextTokenScanner scanner;
-	private ArrayList<String> quotedTokenArray;
-	private ISourceViewer viewer;
-	private ITextResource tr;
-	private String tText = "";
-	private StyledText text;
+	private EMFTextTokenScanner tokenScanner;
+	private List<String> quotedTokenArray;
+	private ISourceViewer sourceViewer;
+	private ITextResource textResource;
+	private String tokenText = "";
+	private StyledText textWidget;
 
 	public String getTokenText() {
-		return tText;
+		return tokenText;
 	}
 
-	public Occurrence(ITextResource textresource, ISourceViewer sourceviewer, ColorManager colorManager,
-			EMFTextTokenScanner aTScanner) {
+	public Occurrence(ITextResource textResource, ISourceViewer sourceViewer, ColorManager colorManager,
+			EMFTextTokenScanner tokenScanner) {
+		// TODO hoang-kim is this call needed?
 		EMFTextRuntimeUIPlugin.getDefault().getPreferenceStore();
-		tr = textresource;
-		viewer = sourceviewer;
-		text = sourceviewer.getTextWidget();
+		this.textResource = textResource;
+		this.sourceViewer = sourceViewer;
+		this.textWidget = sourceViewer.getTextWidget();
 
 		quotedTokenArray = new ArrayList<String>();
-		String[] tokenNames = tr.getMetaInformation().getTokenNames();
+		String[] tokenNames = textResource.getMetaInformation().getTokenNames();
 		for (String tokenName : tokenNames) {
+			// TODO this is ANTLR specific
 			if (tokenName.startsWith("'") && tokenName.endsWith("'")) {
 				quotedTokenArray.add(tokenName.substring(1, tokenName.length() - 1).trim());
 			}
 		}
 
-		scanner = aTScanner;
+		this.tokenScanner = tokenScanner;
 	}
 
-	private EObject getResolvedEObject(EObject eobject) {
-		return eobject.eIsProxy() ? EcoreUtil.resolve(eobject, tr) : eobject;
+	private EObject getResolvedEObject(EObject eObject) {
+		return eObject.eIsProxy() ? EcoreUtil.resolve(eObject, textResource) : eObject;
 	}
 
-	public EObject tryToResolve(List<EObject> eoList) {
-		for (EObject e : eoList) {
-			if (e.eIsProxy())
-				return getResolvedEObject(e);
+	public EObject tryToResolve(List<EObject> objects) {
+		for (EObject object : objects) {
+			if (object.eIsProxy()) {
+				return getResolvedEObject(object);
+			}
 		}
 		return null;
 	}
 
-	private int getEOLength(EObject eobject) {
-		ILocationMap lm = tr.getLocationMap();
-		return lm.getCharEnd(eobject) - lm.getCharStart(eobject) + 1;
+	private int getLength(EObject eObject) {
+		ILocationMap locationMap = textResource.getLocationMap();
+		return locationMap.getCharEnd(eObject) - locationMap.getCharStart(eObject) + 1;
 	}
 
-	public void handleOccurrenceHighlighting(BracketSet brackets) {
+	public void handleOccurrenceHighlighting(BracketSet bracketSet) {
 		// TODO because of the TextResource bug, only available if the editor is
 		// not dirty.
-		if (PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor().isDirty())
+		// TODO remove this once the background parsing is active
+		if (PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor().isDirty()) {
 			return;
-		ProjectionViewer pviewer = null;
-		if (viewer instanceof ProjectionViewer) {
-			pviewer = (ProjectionViewer) viewer;
 		}
-		int caret = text.getCaretOffset();
-		if (pviewer != null) {
-			caret = pviewer.widgetOffset2ModelOffset(caret);
-			if (caret == -1)
+		ProjectionViewer projectionViewer = null;
+		if (sourceViewer instanceof ProjectionViewer) {
+			projectionViewer = (ProjectionViewer) sourceViewer;
+		}
+		int caretOffset = textWidget.getCaretOffset();
+		if (projectionViewer != null) {
+			caretOffset = projectionViewer.widgetOffset2ModelOffset(caretOffset);
+			if (caretOffset == -1) {
 				return;
+			}
 		}
-		if (caret < 0 || caret >= viewer.getDocument().getLength())
+		if (caretOffset < 0 || caretOffset >= sourceViewer.getDocument().getLength()) {
 			return;
-		ILocationMap lm = tr.getLocationMap();
-		List<EObject> eList = lm.getElementsAt(caret);
+		}
+		ILocationMap locationMap = textResource.getLocationMap();
+		List<EObject> elementsAtOffset = locationMap.getElementsAt(caretOffset);
 
-		EObject firstEO = eList.get(0);
-		EObject resolvedEO = tryToResolve(eList);
-		if (resolvedEO != null)
-			eList = lm.getElementsAt(lm.getCharStart(resolvedEO));
+		EObject firstElementAtOffset = elementsAtOffset.get(0);
+		EObject resolvedEO = tryToResolve(elementsAtOffset);
+		if (resolvedEO != null) {
+			elementsAtOffset = locationMap.getElementsAt(locationMap.getCharStart(resolvedEO));
+		}
 
-		scanner.setRange(viewer.getDocument(), lm.getCharStart(firstEO), getEOLength(firstEO));
-		IToken t = scanner.nextToken();
-		while (!t.isEOF()) {
-			if (scanner.getTokenOffset() <= caret && scanner.getTokenLength() + scanner.getTokenOffset() > caret) {
-				if (scanner.getTokenText().trim().equals(""))// the rejected
-																// elements
+		tokenScanner.setRange(sourceViewer.getDocument(), locationMap.getCharStart(firstElementAtOffset), getLength(firstElementAtOffset));
+		IToken token = tokenScanner.nextToken();
+		while (!token.isEOF()) {
+			int tokenOffset = tokenScanner.getTokenOffset();
+			int tokenLength = tokenScanner.getTokenLength();
+			String text = tokenScanner.getTokenText();
+			if (tokenOffset <= caretOffset && tokenLength + tokenOffset > caretOffset) {
+				if (text.trim().equals("")) {
+					// the rejected elements
 					return;
-				tText = scanner.getTokenText();
+				}
+				tokenText = text;
 				break;
 			}
-			t = scanner.nextToken();
+			token = tokenScanner.nextToken();
 		}
 
-		if (tText == null || tText.equals(""))
+		if (tokenText == null || tokenText.equals("")) {
 			return;
-		if ((resolvedEO == null && quotedTokenArray.contains(tText))
-				|| (resolvedEO == null && eList.get(0).eResource() == null) || brackets.isBracket(tText)) {
-			tText = "";
+		}
+		if ((resolvedEO == null && quotedTokenArray.contains(tokenText))
+				|| (resolvedEO == null && elementsAtOffset.get(0).eResource() == null) || bracketSet.isBracket(tokenText)) {
+			tokenText = "";
 			return;
 		}
 		try {
-			setHighlightingPositions(resolvedEO, eList);
+			setHighlightingPositions(resolvedEO, elementsAtOffset);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	// TODO hoang-kim given meaningful names to the parameters
 	private void setHighlightingPositions(EObject eo, List<EObject> eList) {
-		IDocument doc = viewer.getDocument();
-		ILocationMap lm = tr.getLocationMap();
-		IToken t;
+		IDocument document = sourceViewer.getDocument();
+		ILocationMap locationMap = textResource.getLocationMap();
+		IToken token;
 		int defPosition = -1;
 		boolean isNull = eo == null;
-		if (isNull)
+		if (isNull) {
 			eo = eList.get(0);
-		if (eo.eResource().equals(tr)) {
-			scanner.setRange(viewer.getDocument(), lm.getCharStart(eo), getEOLength(eo));
-			t = scanner.nextToken();
-			while (!t.isEOF()) {
-				String tokenText = scanner.getTokenText();
-				if (tokenText.equals(tText)) {
-					doc.addPositionCategory(ExtensionConstants.POSITION_CATEGORY_DEF);
-					try {
-						defPosition = scanner.getTokenOffset();
-						doc.addPosition(ExtensionConstants.POSITION_CATEGORY_DEF, new Position(
-								scanner.getTokenOffset(), scanner.getTokenLength()));
-					} catch (BadLocationException e) {
-						e.printStackTrace();
-					} catch (BadPositionCategoryException e) {
-						e.printStackTrace();
-					}
+		}
+		if (eo.eResource().equals(textResource)) {
+			tokenScanner.setRange(sourceViewer.getDocument(), locationMap.getCharStart(eo), getLength(eo));
+			token = tokenScanner.nextToken();
+			while (!token.isEOF()) {
+				String text = tokenScanner.getTokenText();
+				if (text.equals(tokenText)) {
+					document.addPositionCategory(ExtensionConstants.POSITION_CATEGORY_DEF);
+					defPosition = tokenScanner.getTokenOffset();
+					addPosition(document, ExtensionConstants.POSITION_CATEGORY_DEF);
 					break;
 				}
-				t = scanner.nextToken();
+				token = tokenScanner.nextToken();
 			}
 		}
-		scanner.setRange(viewer.getDocument(), 0, viewer.getDocument().getLength());
+		tokenScanner.setRange(sourceViewer.getDocument(), 0, sourceViewer.getDocument().getLength());
 		EObject occEO;
-		t = scanner.nextToken();
-		doc.addPositionCategory(ExtensionConstants.POSITION_CATEGORY_USE);
-		while (!t.isEOF()) {
-			String tokenText = scanner.getTokenText();
-			if (tokenText != null && tokenText.equals(tText) && scanner.getTokenOffset() != defPosition) {
-				occEO = tryToResolve(lm.getElementsAt(scanner.getTokenOffset()));
-				if (occEO != null)
+		token = tokenScanner.nextToken();
+		document.addPositionCategory(ExtensionConstants.POSITION_CATEGORY_USE);
+		while (!token.isEOF()) {
+			String text = tokenScanner.getTokenText();
+			if (text != null && text.equals(tokenText) && tokenScanner.getTokenOffset() != defPosition) {
+				occEO = tryToResolve(locationMap.getElementsAt(tokenScanner.getTokenOffset()));
+				if (occEO != null) {
 					if ((isNull && eList.contains(occEO)) || !isNull && eo.equals(occEO)) {
-						try {
-							doc.addPosition(ExtensionConstants.POSITION_CATEGORY_USE, new Position(scanner
-									.getTokenOffset(), scanner.getTokenLength()));
-						} catch (BadLocationException e) {
-							e.printStackTrace();
-						} catch (BadPositionCategoryException e) {
-							e.printStackTrace();
-						}
+						addPosition(document, ExtensionConstants.POSITION_CATEGORY_USE);
 					}
+				}
 			}
-			t = scanner.nextToken();
+			token = tokenScanner.nextToken();
+		}
+	}
+
+	private void addPosition(IDocument document, String positionCategory) {
+		try {
+			int tokenOffset = tokenScanner.getTokenOffset();
+			int tokenLength = tokenScanner.getTokenLength();
+			Position position = new Position(tokenOffset, tokenLength);
+			document.addPosition(positionCategory, position);
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		} catch (BadPositionCategoryException e) {
+			e.printStackTrace();
 		}
 	}
 
