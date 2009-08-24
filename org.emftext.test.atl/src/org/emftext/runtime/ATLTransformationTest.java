71a3c2689883773c043515ceeeece4e2e6a9946f
Christian Wende <christian.wende@devboost.de>
2009-08-24 14:24:49 +0000

    move atl test to separate plug-in

---
+++ org.emftext.test.atl/src/org/emftext/runtime/ATLTransformationTest.java
@@ -0,0 +1,61 @@
+package org.emftext.runtime;
+
+import static org.emftext.test.ConcreteSyntaxTestHelper.registerResourceFactories;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.emftext.runtime.atl.ATLTransformationPostProcessor;
+import org.emftext.sdk.concretesyntax.ConcreteSyntax;
+import org.emftext.sdk.concretesyntax.resource.cs.CsResourceFactory;
+import org.emftext.language.ecore.resource.text.TextEcoreResourceFactory;
+import org.junit.Before;
+
+public class ATLTransformationTest extends TestCase {
+
+	@Before
+	public void setUp() {
+		registerResourceFactories();
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				"ecore", new TextEcoreResourceFactory());
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("cs",
+				new CsResourceFactory());
+
+	}
+
+	public void testTransformationResult() throws Exception {
+
+		String testfile = "platform:/resource/org.emftext.test.atl/model/example.cs";
+		loadResourceWithProcessor(testfile);
+		
+		String outfile = "platform:/resource/org.emftext.test.atl/model/example.out.cs";
+		Resource outResource = loadResource(outfile);
+		assertEquals(1, outResource.getContents().size());
+		assertEquals("wasHere", ((ConcreteSyntax) outResource.getContents().get(0) ).getName());
+		
+	}
+	
+	private Resource loadResource(String grammar) throws IOException {
+		URI fileUri = URI.createURI(grammar);
+
+		ResourceSet rs = new ResourceSetImpl();
+		Resource csResource = rs.getResource(fileUri, true);
+		return csResource;
+	}
+
+	private Resource loadResourceWithProcessor(String grammar) throws IOException {
+		URI fileUri = URI.createURI(grammar);
+
+		ResourceSet rs = new ResourceSetImpl();
+
+		ATLTransformationPostProcessor transformationPostProcessor = new TestTransformationPostProcessor();
+		rs.getLoadOptions().putAll(transformationPostProcessor.getOptions());
+		Resource csResource = rs.getResource(fileUri, true);
+		return csResource;
+	}
+}
---
+++ org.emftext.test.atl/src/org/emftext/runtime/TestTransformationPostProcessor.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.emftext.runtime;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.emftext.runtime.atl.ATLTransformationPostProcessor;
+
+final class TestTransformationPostProcessor extends
+		ATLTransformationPostProcessor {
+	@Override
+	protected Map<String, String> getMetamodelURIs() {
+		Map<String, String> metamodels = new HashMap<String, String>();
+		metamodels.put("ConcreteSyntax",
+				"http://www.emftext.org/sdk/concretesyntax");
+		return metamodels;
+	}
+
+	@Override
+	protected String getTransformationURI() {
+		return "platform:/resource/org.emftext.test.atl/transformations/assimilator.asm";
+	}
+
+	@Override
+	protected String getInMetamodelName() {
+		return "ConcreteSyntax";
+	}
+
+	@Override
+	protected String getOutMetamodelName() {
+		return "ConcreteSyntax";
+	}
+
+	@Override
+	protected String getOutputURI() {
+		String fileExtension = currentResource.getURI().fileExtension();
+		String currentUri = currentResource.getURI().toPlatformString(true);
+		String newUri = currentUri.substring(0, currentUri.length() - fileExtension.length() - 1);
+		newUri += ".out.cs";
+		return newUri;
+	}
+}
