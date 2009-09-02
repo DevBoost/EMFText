0019b624b75308874fdb913943e89b8e694b2adc
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-02 09:47:13 +0000

    replaced paths to grammar file with search function

--- org.emftext.test/src/org/emftext/sdk/codegen/regex/AntlrTranslationTest.java
+++ org.emftext.test/src/org/emftext/sdk/codegen/regex/AntlrTranslationTest.java
@@ -3,7 +3,10 @@ package org.emftext.sdk.codegen.regex;
 import static org.emftext.test.ConcreteSyntaxTestHelper.registerResourceFactories;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,55 +29,6 @@ public class AntlrTranslationTest extends TestCase {
 	public void setUp() {
 		registerResourceFactories();
 	}
-
-	public static String[] grammars = new String[] {
-				"../../EMFText Languages/org.emftext.language.bool/metamodel/bool.cs",
-				"../../EMFText Languages/org.emftext.language.c_sharp/metamodel/c_sharp.cs",
-				"../../EMFText Languages/org.emftext.language.simple_c/metamodel/c.cs",
-				"../../EMFText Languages/org.emftext.language.c_sharp.test/CSharpSyntaxCheck/CheckCSSyntax.cs",
-				"../../EMFText Languages/org.emftext.language.chess/metamodel/chess.cs",
-				"../../EMFText Languages/org.emftext.language.conference/metamodel/conference.cs",
-				"../../EMFText Languages/org.emftext.language.custom_sandwich/metamodel/custom_sandwich.cs",
-				"../../EMFText Languages/org.emftext.language.customer/metamodel/customer.cs",
-				"../../EMFText Languages/org.emftext.language.db_schema/metamodel/db_schema.cs",
-				"../../EMFText Languages/org.emftext.language.dot/metamodel/dot.cs",
-				"../../EMFText Languages/org.emftext.language.ecore.resource.facade/metamodel/facade.ecore.cs",
-				"../../EMFText Languages/org.emftext.language.featherweight_java/metamodel/featherweight_java.cs",
-				"../../EMFText Languages/org.emftext.language.feature/metamodel/feature.cs",
-				"../../EMFText Languages/org.emftext.language.filesystem/metamodel/filesystem.cs",
-				"../../EMFText Languages/org.emftext.language.forms/metamodel/forms.cs",
-				"../../EMFText Languages/org.emftext.language.forms/standardSyntax/forms.cs",
-				"../../EMFText Languages/org.emftext.language.formular/metamodel/FormularMM.cs",
-				"../../EMFText Languages/org.emftext.language.java_templates/metamodel/java_templates.cs",
-				"../../EMFText Languages/org.emftext.language.java/metamodel/java.cs",
-				"../../EMFText Languages/org.emftext.language.java.javabehavior4uml/metamodel/JavaBehavior4UML.cs",
-				"../../EMFText Languages/org.emftext.language.km3/metamodel/KM3.cs",
-				"../../EMFText Languages/org.emftext.language.models/metamodel/models.cs",
-				"../../EMFText Languages/org.emftext.language.office/metamodel/office.cs",
-				"../../EMFText Languages/org.emftext.language.owl/metamodel/owl.cs",
-				"../../EMFText Languages/org.emftext.language.parametercheck/metamodel/parametercheck.cs",
-				"../../EMFText Languages/org.emftext.language.pico/metamodel/pico.cs",
-				"../../EMFText Languages/org.emftext.language.quickuml/metamodel/quickuml.cs",
-				"../../EMFText Languages/org.emftext.language.regexp/metamodel/regular_expressions.cs",
-				"../../EMFText Languages/org.emftext.language.java.reusejava/metamodel/reusejava.cs",
-				"../../EMFText Languages/org.emftext.language.sandwich/metamodel/sandwich.cs",
-				"../../EMFText Languages/org.emftext.language.simple_gui/metamodel/simple_gui.cs",
-				"../../EMFText Languages/org.emftext.language.simple_math/metamodel/simple_math.cs",
-				"../../EMFText Languages/org.emftext.language.sparql/metamodel/SPARQL.cs",
-				"../../EMFText Languages/org.emftext.language.statemachine/metamodel/statemachine.cs",
-				"../../EMFText Languages/org.emftext.language.tbool/metamodel/tbool.cs",
-				"../../EMFText Languages/org.emftext.language.template_concepts.call/metamodel/template_call.cs",
-				"../../EMFText Languages/org.emftext.language.ecore.resource.text/metamodel/text.ecore.cs",
-				"../../EMFText Languages/org.emftext.language.textadventure/metamodel/textadventure.cs",
-				"../../EMFText Languages/org.emftext.language.threevaluedlogic/metamodel/threevaluedlogic.cs",
-				"../../EMFText Languages/org.emftext.language.java.treejava/metamodel/treejava.cs",
-				"../../EMFText Languages/org.emftext.language.uml_class/metamodel/uml_class.cs",
-				"../../EMFText Languages/org.emftext.language.usecaseinvariant/metamodel/UseCaseInvariant.cs",
-				"../../EMFText Languages/org.emftext.language.valueflow/metamodel/valueflow.cs",
-				"../../EMFText Languages/org.emftext.language.xml/metamodel/xml.cs"};
-
-	
-
 	
 	@Test
 	public void testWhitespaceTranslation() {
@@ -96,7 +50,13 @@ public class AntlrTranslationTest extends TestCase {
 	@Test
 	public void testExpsFromGrammars() throws IOException, RecognitionException {
 		
+		Collection<String> grammars = findAllGrammars(new File(".."));
+		System.out.println("AntlrTranslationTest.testExpsFromGrammars() found " + grammars.size() + " grammar files.");
+		// make sure all grammars are found
+		assertTrue(grammars.size() > 140);
+		
 		for (String grammar : grammars) {
+			System.out.println("AntlrTranslationTest.testExpsFromGrammars() testing " + grammar);
 			Resource resource = loadResource(grammar);
 
 			TreeIterator<EObject> contents = resource.getAllContents();
@@ -110,6 +70,32 @@ public class AntlrTranslationTest extends TestCase {
 		}
 
 	}
+
+	private Collection<String> findAllGrammars(File directory) {
+		Collection<String> grammarPaths = new LinkedHashSet<String>();
+		File[] subDirs = directory.listFiles(new FileFilter() {
+
+			public boolean accept(File file) {
+				return file.isDirectory() && !file.getName().startsWith(".");
+			}
+			
+		});
+		File[] grammarFiles = directory.listFiles(new FileFilter() {
+
+			public boolean accept(File file) {
+				return !file.isDirectory() && file.getName().endsWith(".cs");
+			}
+			
+		});
+		for (File file : grammarFiles) {
+			grammarPaths.add(file.getAbsolutePath());
+		}
+		for (File subDir : subDirs) {
+			grammarPaths.addAll(findAllGrammars(subDir));
+		}
+		return grammarPaths;
+	}
+
 
 	private Resource loadResource(String grammar) throws IOException {
 
