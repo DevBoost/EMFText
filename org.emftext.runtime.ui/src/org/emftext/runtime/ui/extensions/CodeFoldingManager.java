2006abf57ef6222f6343d5298d28a52ff9f28130
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-08 07:37:10 +0000

    removed annotations that are incompatible with J2SE5

--- org.emftext.runtime.ui/src/org/emftext/runtime/ui/extensions/CodeFoldingManager.java
+++ org.emftext.runtime.ui/src/org/emftext/runtime/ui/extensions/CodeFoldingManager.java
@@ -103,15 +103,12 @@ public class CodeFoldingManager {
 			this.uri = uri;
 		}
 
-		@Override
 		public void partActivated(IWorkbenchPartReference partRef) {
 		}
 
-		@Override
 		public void partBroughtToTop(IWorkbenchPartReference partRef) {
 		}
 
-		@Override
 		public void partClosed(IWorkbenchPartReference partRef) {
 			if (partRef.isDirty())
 				return;
@@ -128,23 +125,18 @@ public class CodeFoldingManager {
 
 		}
 
-		@Override
 		public void partDeactivated(IWorkbenchPartReference partRef) {
 		}
 
-		@Override
 		public void partHidden(IWorkbenchPartReference partRef) {
 		}
 
-		@Override
 		public void partInputChanged(IWorkbenchPartReference partRef) {
 		}
 
-		@Override
 		public void partOpened(IWorkbenchPartReference partRef) {
 		}
 
-		@Override
 		public void partVisible(IWorkbenchPartReference partRef) {
 		}
 
