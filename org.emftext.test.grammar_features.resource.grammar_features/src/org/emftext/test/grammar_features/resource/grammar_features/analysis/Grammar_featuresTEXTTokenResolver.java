8981007243db900afa734833b7231eada5fe6602
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-11 18:25:19 +0000

    regenerated resolvers to remove deprecation warnings

--- org.emftext.test.grammar_features.resource.grammar_features/src/org/emftext/test/grammar_features/resource/grammar_features/analysis/Grammar_featuresTEXTTokenResolver.java
+++ org.emftext.test.grammar_features.resource.grammar_features/src/org/emftext/test/grammar_features/resource/grammar_features/analysis/Grammar_featuresTEXTTokenResolver.java
@@ -1,15 +1,20 @@
 package org.emftext.test.grammar_features.resource.grammar_features.analysis;
 
-public class Grammar_featuresTEXTTokenResolver extends org.emftext.runtime.resource.impl.JavaBasedTokenResolver implements org.emftext.runtime.resource.ITokenResolver {
+public class Grammar_featuresTEXTTokenResolver extends org.emftext.runtime.resource.impl.AbstractTokenResolver {
 	
-	@Override	
+	private org.emftext.test.grammar_features.resource.grammar_features.Grammar_featuresDefaultTokenResolver defaultTokenResolver = new org.emftext.test.grammar_features.resource.grammar_features.Grammar_featuresDefaultTokenResolver();
+	
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = super.deResolve(value, feature, container);
+		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
 		return result;
 	}
 	
-	@Override	
 	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.runtime.resource.ITokenResolveResult result) {
-		super.resolve(lexem, feature, result);
+		defaultTokenResolver.resolve(lexem, feature, result);
 	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		defaultTokenResolver.setOptions(options);
+	}
+	
 }
