84415d87806c7c00476410758612bc12ac98f285
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-16 13:04:06 +0000

    added documentation

--- org.emftext.runtime/src/org/emftext/runtime/resource/impl/TerminateParsingException.java
+++ org.emftext.runtime/src/org/emftext/runtime/resource/impl/TerminateParsingException.java
@@ -1,6 +1,10 @@
 package org.emftext.runtime.resource.impl;
 
-// TODO add documentation
+/**
+ * This exception is used to terminate generated parsers. Depending on
+ * the state of a stop flag the parser throw this exception to break 
+ * the control flow and allow users of the parser to stop parsing.
+ */
 public class TerminateParsingException extends RuntimeException {
 
 	private static final long serialVersionUID = 117529647036954724L;
