21dc20f8c14893269ee6c1f935dfa3cf3c4c9350
Mirko Seifert <mirko.seifert@devboost.de>
2009-10-05 15:47:18 +0000

    removed current runtime plug-ins (not needed anymore)

--- org.emftext.runtime/src/org/emftext/runtime/resource/IHoverTextProvider.java
+++
@@ -1,8 +0,0 @@
-package org.emftext.runtime.resource;
-
-import org.eclipse.emf.ecore.EObject;
-
-public interface IHoverTextProvider {
-	
-	public String getHoverText(EObject object);
-}
--- org.emftext.runtime/src/org/emftext/runtime/resource/IProblem.java
+++
@@ -1,6 +0,0 @@
-package org.emftext.runtime.resource;
-
-public interface IProblem {
-	public String getMessage();
-	public EProblemType getType();
-}
