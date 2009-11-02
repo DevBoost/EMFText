40bad5e7a0c5497a8ae893f362a27fc3a6d00428
Mirko Seifert <mirko.seifert@devboost.de>
2009-11-02 17:59:23 +0000

    moving EMFText OCL plug-in to utility folder

--- org.emftext.runtime.ocl/src/org/emftext/runtime/ocl/IProblemHandler.java
+++
@@ -1,8 +0,0 @@
-package org.emftext.runtime.ocl;
-
-import org.eclipse.emf.ecore.EObject;
-
-public interface IProblemHandler {
-
-	public void addProblem(String message, EObject cause, boolean isError);
-}
