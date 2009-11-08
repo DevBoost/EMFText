f10aeae2c49e063d194b1b5465b8f9754817eb57
Mirko Seifert <mirko.seifert@devboost.de>
2009-11-08 23:04:39 +0000

    regenerated CS bracket preference page

--- org.emftext.sdk.concretesyntax.resource.cs/src-gen/org/emftext/sdk/concretesyntax/resource/cs/ui/CsBracketPreferencePage.java
+++ org.emftext.sdk.concretesyntax.resource.cs/src-gen/org/emftext/sdk/concretesyntax/resource/cs/ui/CsBracketPreferencePage.java
@@ -34,7 +34,6 @@ public class CsBracketPreferencePage extends org.eclipse.jface.preference.Prefer
 	private org.eclipse.swt.widgets.Button enableCheckbox;
 	private org.eclipse.swt.widgets.Button enableClosingInside;
 	private org.eclipse.swt.widgets.Button matchingBracketsColorButton;
-	private org.eclipse.swt.widgets.Combo languagesCombo;
 	private org.eclipse.swt.widgets.Label bracketTokensLabel;
 	private org.eclipse.swt.widgets.Combo leftBracketTokensCombo;
 	private org.eclipse.swt.widgets.Combo rightBracketTokensCombo;
@@ -42,13 +41,13 @@ public class CsBracketPreferencePage extends org.eclipse.jface.preference.Prefer
 	private org.eclipse.swt.widgets.Button addBracketButton;
 	private org.eclipse.swt.widgets.Button removeBracketButton;
 	private java.util.Map<String, String> bracketSetTemp = new java.util.HashMap<String, String>();
-	private String language;
+	private String language = new org.emftext.sdk.concretesyntax.resource.cs.mopp.CsMetaInformation().getSyntaxName();
 	
 	private org.emftext.sdk.concretesyntax.resource.cs.ui.CsBracketSet bracketsTmp;
 	
-	//*
+	//
 	// Creates a preference page for bracket setting.
-	///
+	//
 	public CsBracketPreferencePage() {
 		super();
 		
@@ -71,7 +70,8 @@ public class CsBracketPreferencePage extends org.eclipse.jface.preference.Prefer
 		}
 	}
 	
-	@Override	protected org.eclipse.swt.widgets.Control createContents(org.eclipse.swt.widgets.Composite parent) {
+	@Override	
+	protected org.eclipse.swt.widgets.Control createContents(org.eclipse.swt.widgets.Composite parent) {
 		
 		// outer Composite
 		org.eclipse.swt.widgets.Composite settingComposite = new org.eclipse.swt.widgets.Composite(parent, org.eclipse.swt.SWT.NONE);
@@ -102,24 +102,7 @@ public class CsBracketPreferencePage extends org.eclipse.jface.preference.Prefer
 		gd = new org.eclipse.swt.layout.GridData(org.eclipse.swt.layout.GridData.HORIZONTAL_ALIGN_BEGINNING);
 		matchingBracketsColorButton.setLayoutData(gd);
 		
-		org.eclipse.swt.widgets.Composite languageComposite = new org.eclipse.swt.widgets.Composite(settingComposite, org.eclipse.swt.SWT.NONE);
-		layout = new org.eclipse.swt.layout.GridLayout();
-		layout.marginHeight = 0;
-		layout.marginWidth = 0;
-		layout.numColumns = 2;
-		gd = new org.eclipse.swt.layout.GridData(org.eclipse.swt.layout.GridData.BEGINNING);
-		gd.horizontalSpan = 2;
-		gd.verticalIndent = 30;
-		languageComposite.setLayout(layout);
-		languageComposite.setLayoutData(gd);
-		
-		languagesCombo = new org.eclipse.swt.widgets.Combo(languageComposite, org.eclipse.swt.SWT.DROP_DOWN		| org.eclipse.swt.SWT.READ_ONLY | org.eclipse.swt.SWT.LEFT);
-		gd = new org.eclipse.swt.layout.GridData(org.eclipse.swt.layout.GridData.BEGINNING);
-		languagesCombo.setLayoutData(gd);
-		languagesCombo.setVisible(false);
-		
-		org.eclipse.swt.widgets.Composite tokenSelectionComposite = new org.eclipse.swt.widgets.Composite(settingComposite,
-		org.eclipse.swt.SWT.NONE);
+		org.eclipse.swt.widgets.Composite tokenSelectionComposite = new org.eclipse.swt.widgets.Composite(settingComposite, org.eclipse.swt.SWT.NONE);
 		layout = new org.eclipse.swt.layout.GridLayout();
 		layout.numColumns = 3;
 		layout.marginHeight = 0;
@@ -133,20 +116,27 @@ public class CsBracketPreferencePage extends org.eclipse.jface.preference.Prefer
 		bracketTokensLabel = new org.eclipse.swt.widgets.Label(tokenSelectionComposite, org.eclipse.swt.SWT.LEFT);
 		gd = new org.eclipse.swt.layout.GridData(org.eclipse.swt.layout.GridData.BEGINNING);
 		gd.horizontalSpan = 3;
-		bracketTokensLabel.setText("Select the desired bracket pair");
+		bracketTokensLabel.setText("Add new bracket pair");
 		bracketTokensLabel.setLayoutData(gd);
 		
-		leftBracketTokensCombo = new org.eclipse.swt.widgets.Combo(tokenSelectionComposite,
-		org.eclipse.swt.SWT.DROP_DOWN | org.eclipse.swt.SWT.READ_ONLY);
+		leftBracketTokensCombo = new org.eclipse.swt.widgets.Combo(tokenSelectionComposite,org.eclipse.swt.SWT.DROP_DOWN | org.eclipse.swt.SWT.READ_ONLY);
 		gd = new org.eclipse.swt.layout.GridData(org.eclipse.swt.layout.GridData.BEGINNING);
 		leftBracketTokensCombo.setLayoutData(gd);
 		
-		rightBracketTokensCombo = new org.eclipse.swt.widgets.Combo(tokenSelectionComposite,
-		org.eclipse.swt.SWT.DROP_DOWN | org.eclipse.swt.SWT.READ_ONLY);
+		rightBracketTokensCombo = new org.eclipse.swt.widgets.Combo(tokenSelectionComposite,org.eclipse.swt.SWT.DROP_DOWN | org.eclipse.swt.SWT.READ_ONLY);
 		gd = new org.eclipse.swt.layout.GridData(org.eclipse.swt.layout.GridData.FILL);
-		gd.horizontalSpan = 2;
 		rightBracketTokensCombo.setLayoutData(gd);
 		
+		addBracketButton = new org.eclipse.swt.widgets.Button(tokenSelectionComposite, org.eclipse.swt.SWT.PUSH);
+		addBracketButton.setText("Add");
+		addBracketButton.setLayoutData(new org.eclipse.swt.layout.GridData(org.eclipse.swt.layout.GridData.BEGINNING, org.eclipse.swt.layout.GridData.BEGINNING, false, false));
+		
+		org.eclipse.swt.widgets.Label configurePairsLabel = new org.eclipse.swt.widgets.Label(tokenSelectionComposite, org.eclipse.swt.SWT.LEFT);
+		gd = new org.eclipse.swt.layout.GridData(org.eclipse.swt.layout.GridData.BEGINNING);
+		gd.horizontalSpan = 3;
+		gd.verticalIndent = 20;
+		configurePairsLabel.setText("Configure bracket pairs");
+		configurePairsLabel.setLayoutData(gd);
 		bracketsList = new org.eclipse.swt.widgets.List(tokenSelectionComposite, org.eclipse.swt.SWT.MULTI);
 		gd = new org.eclipse.swt.layout.GridData(org.eclipse.swt.layout.GridData.CENTER, org.eclipse.swt.layout.GridData.FILL, false, true);
 		gd.horizontalSpan = 2;
@@ -157,13 +147,8 @@ public class CsBracketPreferencePage extends org.eclipse.jface.preference.Prefer
 		
 		enableClosingInside = new org.eclipse.swt.widgets.Button(tokenSelectionComposite, org.eclipse.swt.SWT.CHECK);
 		enableClosingInside.setText("Enable closing inside");
-		enableClosingInside		.setToolTipText("If this option is enabled, other bracket pair can close inside this pair automatically.");
+		enableClosingInside.setToolTipText("If this option is enabled, other bracket pair can close inside this pair automatically.");
 		enableClosingInside.setLayoutData(new org.eclipse.swt.layout.GridData(org.eclipse.swt.layout.GridData.BEGINNING,
-		org.eclipse.swt.layout.GridData.BEGINNING, false, false));
-		
-		addBracketButton = new org.eclipse.swt.widgets.Button(tokenSelectionComposite, org.eclipse.swt.SWT.PUSH);
-		addBracketButton.setText("Add");
-		addBracketButton.setLayoutData(new org.eclipse.swt.layout.GridData(org.eclipse.swt.layout.GridData.BEGINNING,
 		org.eclipse.swt.layout.GridData.BEGINNING, false, false));
 		
 		removeBracketButton = new org.eclipse.swt.widgets.Button(tokenSelectionComposite, org.eclipse.swt.SWT.PUSH);
@@ -178,9 +163,9 @@ public class CsBracketPreferencePage extends org.eclipse.jface.preference.Prefer
 		return settingComposite;
 	}
 	
-	//*
+	//
 	// Initialize and handle the values of this preference page
-	///
+	//
 	private void handleMatchingBracketsSelection() {
 		// not for the case of none existing language
 		enableCheckbox.setSelection(getPreferenceStore().getBoolean(		org.emftext.sdk.concretesyntax.resource.cs.ui.CsPreferenceConstants.EDITOR_MATCHING_BRACKETS_CHECKBOX));
@@ -190,39 +175,23 @@ public class CsBracketPreferencePage extends org.eclipse.jface.preference.Prefer
 		BRACKETS_COLOR);
 		matchingBracketsColorEditor.setColorValue(rgb);
 		
-		String extension = null;
-		org.eclipse.ui.IWorkbench workbench = org.eclipse.ui.PlatformUI.getWorkbench();
-		final org.eclipse.ui.IEditorPart activeEditor = workbench.getActiveWorkbenchWindow()		.getActivePage().getActiveEditor();
-		if (activeEditor != null) {
-			if (activeEditor instanceof org.emftext.sdk.concretesyntax.resource.cs.ui.CsEditor) {
-				org.emftext.sdk.concretesyntax.resource.cs.ui.CsEditor emfTextEditor = (org.emftext.sdk.concretesyntax.resource.cs.ui.CsEditor) activeEditor;
-				org.emftext.sdk.concretesyntax.resource.cs.ICsTextResource resource = (org.emftext.sdk.concretesyntax.resource.cs.ICsTextResource) emfTextEditor.getResource();
-				extension = resource.getMetaInformation().getSyntaxName();
-			}
-		}
-		
-		languagesCombo.setItems(languageIDs.toArray(new String[0]));
-		languagesCombo.select(0);
-		if (extension != null) {
-			int idx = languagesCombo.indexOf(extension);
-			if (idx > -1) {
-				languagesCombo.select(idx);
-			} else {
-				idx = 0;
-			}
-		}
-		language = languagesCombo.getItem(languagesCombo.getSelectionIndex());
-		leftBracketTokensCombo.setItems(ALL_LEFT_BRACKETS);
-		leftBracketTokensCombo.select(0);
-		rightBracketTokensCombo.setItems(ALL_RIGHT_BRACKETS);
-		rightBracketTokensCombo.select(0);
-		bracketsTmp.setBrackets(getPreferenceStore().getString(		language + org.emftext.sdk.concretesyntax.resource.cs.ui.CsPreferenceConstants.EDITOR_BRACKETS_SUFFIX));
+		initializeLanguage();
+		bracketsTmp.setBrackets(getPreferenceStore().getString(language + org.emftext.sdk.concretesyntax.resource.cs.ui.CsPreferenceConstants.EDITOR_BRACKETS_SUFFIX));
 		String[] brackets = bracketsTmp.getBracketArray();
 		if (brackets != null) {
 			bracketsList.setItems(brackets);
 		}
 	}
 	
+	public void initializeLanguage() {
+		bracketSetTemp.put(language, bracketsTmp.getBracketString());
+		bracketsTmp.setBrackets(bracketSetTemp.get(language));
+		leftBracketTokensCombo.setItems(ALL_LEFT_BRACKETS);
+		leftBracketTokensCombo.select(0);
+		rightBracketTokensCombo.setItems(ALL_RIGHT_BRACKETS);
+		rightBracketTokensCombo.select(0);
+		bracketsList.setItems(bracketsTmp.getBracketArray());
+	}
 	private void addListenersToStyleButtons() {
 		enableCheckbox.addSelectionListener(new org.eclipse.swt.events.SelectionListener() {
 			public void widgetDefaultSelected(org.eclipse.swt.events.SelectionEvent e) {
@@ -231,23 +200,6 @@ public class CsBracketPreferencePage extends org.eclipse.jface.preference.Prefer
 			public void widgetSelected(org.eclipse.swt.events.SelectionEvent e) {
 				matchingBracketsColorButton.setEnabled(enableCheckbox.getSelection());
 			}
-		});
-		languagesCombo.addSelectionListener(new org.eclipse.swt.events.SelectionListener() {
-			
-			public void widgetDefaultSelected(org.eclipse.swt.events.SelectionEvent e) {
-			}
-			
-			public void widgetSelected(org.eclipse.swt.events.SelectionEvent e) {
-				bracketSetTemp.put(language, bracketsTmp.getBracketString());
-				language = languagesCombo.getText();
-				bracketsTmp.setBrackets(bracketSetTemp.get(language));
-				leftBracketTokensCombo.setItems(ALL_LEFT_BRACKETS);
-				leftBracketTokensCombo.select(0);
-				rightBracketTokensCombo.setItems(ALL_RIGHT_BRACKETS);
-				rightBracketTokensCombo.select(0);
-				bracketsList.setItems(bracketsTmp.getBracketArray());
-			}
-			
 		});
 		addBracketButton.addSelectionListener(new org.eclipse.swt.events.SelectionListener() {
 			
@@ -346,12 +298,7 @@ public class CsBracketPreferencePage extends org.eclipse.jface.preference.Prefer
 		// set the values after ok or apply
 		org.eclipse.jface.preference.PreferenceConverter.setValue(getPreferenceStore(), BRACKETS_COLOR, matchingBracketsColorEditor.getColorValue());
 		getPreferenceStore().setValue(org.emftext.sdk.concretesyntax.resource.cs.ui.CsPreferenceConstants.EDITOR_MATCHING_BRACKETS_CHECKBOX, enableCheckbox.getSelection());
-		
-		for (String languageID : languagesCombo.getItems()) {
-			getPreferenceStore().setValue(			languageID + org.emftext.sdk.concretesyntax.resource.cs.ui.CsPreferenceConstants.EDITOR_BRACKETS_SUFFIX,
-			bracketSetTemp.get(languageID));
-		}
-		
+		getPreferenceStore().setValue(language + org.emftext.sdk.concretesyntax.resource.cs.ui.CsPreferenceConstants.EDITOR_BRACKETS_SUFFIX, bracketSetTemp.get(language));
 		org.eclipse.ui.IWorkbench workbench = org.eclipse.ui.PlatformUI.getWorkbench();
 		org.eclipse.ui.IEditorPart editor = workbench.getActiveWorkbenchWindow().getActivePage().getActiveEditor();
 		if (editor != null && editor instanceof org.emftext.sdk.concretesyntax.resource.cs.ui.CsEditor) {
