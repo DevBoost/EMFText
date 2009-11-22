a72e61c74e78dfb0cb24244a663fc580e3dd844f
Mirko Seifert <mirko.seifert@devboost.de>
2009-11-22 13:49:56 +0000

    added some documentation

--- org.emftext.sdk.codegen/src/org/emftext/sdk/codegen/generators/code_completion/helpers/IExpectedElement.java
+++ org.emftext.sdk.codegen/src/org/emftext/sdk/codegen/generators/code_completion/helpers/IExpectedElement.java
@@ -1,5 +1,9 @@
 package org.emftext.sdk.codegen.generators.code_completion.helpers;
 
+/**
+ * An empty interface for {@link ExpectedKeyword} and
+ * {@link ExpectedFeature} that is needed to store both
+ * classes in typed collections.
+ */
 public interface IExpectedElement {
-
 }
