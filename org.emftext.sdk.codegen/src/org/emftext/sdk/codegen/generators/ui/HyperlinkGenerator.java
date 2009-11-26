aef79e8f7c0927ec47d86c59ff3c10b89ca6b0a8
Mirko Seifert <mirko.seifert@devboost.de>
2009-11-26 13:41:10 +0000

    fixed bug that prevented opening of the correct editor when clicking on a hyperlink

--- org.emftext.sdk.codegen/src/org/emftext/sdk/codegen/generators/ui/HyperlinkGenerator.java
+++ org.emftext.sdk.codegen/src/org/emftext/sdk/codegen/generators/ui/HyperlinkGenerator.java
@@ -66,27 +66,23 @@ public class HyperlinkGenerator extends JavaBaseGenerator {
 		return true;
 	}
 
-	private void addMethods(
-			org.emftext.sdk.codegen.composites.StringComposite sc) {
+	private void addMethods(StringComposite sc) {
 		addGetHyperlinkTextMethod(sc);
 		addLengthMethod(sc);
 		addGetTypeLabelMethod(sc);
 		addOpenMethod(sc);
-		addIsSupportedMethod(sc);
 		addGetIFileFromResourceMethod(sc);
 		addGetHyperlinkRegionMethod(sc);
 	}
 
-	private void addGetHyperlinkRegionMethod(
-			org.emftext.sdk.codegen.composites.StringComposite sc) {
+	private void addGetHyperlinkRegionMethod(StringComposite sc) {
 		sc.add("public " + I_REGION + " getHyperlinkRegion() {");
 		sc.add("return region;");
 		sc.add("}");
 		sc.addLineBreak();
 	}
 
-	private void addGetIFileFromResourceMethod(
-			org.emftext.sdk.codegen.composites.StringComposite sc) {
+	private void addGetIFileFromResourceMethod(StringComposite sc) {
 		sc.add("private " + I_FILE + " getIFileFromResource() {");
 		sc.add(URI + " resourceURI = linkTarget.eResource().getURI();");
 		sc.add("if (resourceURI.toString().startsWith(\"pathmap\")) {");
@@ -105,18 +101,7 @@ public class HyperlinkGenerator extends JavaBaseGenerator {
 		sc.addLineBreak();
 	}
 
-	private void addIsSupportedMethod(
-			org.emftext.sdk.codegen.composites.StringComposite sc) {
-		sc.add("private boolean isSupported(String fileExtension) {");
-		// TODO this method used to iterate over all registered syntax and
-		// return true if there was a syntax for the given file extension
-		sc.add("return true;");
-		sc.add("}");
-		sc.addLineBreak();
-	}
-
-	private void addOpenMethod(
-			org.emftext.sdk.codegen.composites.StringComposite sc) {
+	private void addOpenMethod(StringComposite sc) {
 		sc.add("// Opens the resource in <code>linkTarget</code> with");
 		sc.add("// EMFText editor, if it supports the file extension of this");
 		sc.add("// resource, and tries to jump to the definition. Else it tries to open with");
@@ -130,14 +115,9 @@ public class HyperlinkGenerator extends JavaBaseGenerator {
 		sc.add(I_WORKBENCH + " workbench = " + PLATFORM_UI + ".getWorkbench();");
 		sc.add(I_WORKBENCH_PAGE + " page = workbench.getActiveWorkbenchWindow().getActivePage();");
 		sc.add("try {");
-		// FIXME the EditorID has to be of EMFTextEditor
 		sc.add(I_EDITOR_PART + " activeEditor = page.getActiveEditor();");
-		sc.add("if (isSupported(file.getFileExtension())) {");
-		sc.add("page.openEditor(new " + FILE_EDITOR_INPUT + "(file), activeEditor.getSite().getId());");
-		sc.add("} else {");
 		sc.add(I_EDITOR_DESCRIPTOR + " desc = workbench.getEditorRegistry().getDefaultEditor(file.getName());");
 		sc.add("page.openEditor(new " + FILE_EDITOR_INPUT + "(file), desc.getId());");
-		sc.add("}");
 		sc.add(I_EDITOR_PART + " editorPart = activeEditor;");
 		sc.add("if (editorPart instanceof " + editorClassName + ") {");
 		sc.add(editorClassName + " emftEditor = (" + editorClassName + ") editorPart;");
@@ -151,16 +131,14 @@ public class HyperlinkGenerator extends JavaBaseGenerator {
 		sc.addLineBreak();
 	}
 
-	private void addGetTypeLabelMethod(
-			org.emftext.sdk.codegen.composites.StringComposite sc) {
+	private void addGetTypeLabelMethod(StringComposite sc) {
 		sc.add("public String getTypeLabel() {");
 		sc.add("return null;");
 		sc.add("}");
 		sc.addLineBreak();
 	}
 
-	private void addLengthMethod(
-			org.emftext.sdk.codegen.composites.StringComposite sc) {
+	private void addLengthMethod(StringComposite sc) {
 		sc.add("// @return the length of the hyperlink text");
 		sc.add("public int length() {");
 		sc.add("return text.length();");
@@ -169,15 +147,14 @@ public class HyperlinkGenerator extends JavaBaseGenerator {
 	}
 
 	private void addGetHyperlinkTextMethod(
-			org.emftext.sdk.codegen.composites.StringComposite sc) {
+			StringComposite sc) {
 		sc.add("public String getHyperlinkText() {");
 		sc.add("return text;");
 		sc.add("}");
 		sc.addLineBreak();
 	}
 
-	private void addConstructor(
-			org.emftext.sdk.codegen.composites.StringComposite sc) {
+	private void addConstructor(StringComposite sc) {
 		sc.add("// Creates the hyperlink.");
 		sc.add("//");
 		sc.add("// @param region");
@@ -192,7 +169,7 @@ public class HyperlinkGenerator extends JavaBaseGenerator {
 		sc.addLineBreak();
 	}
 
-	private void addFields(org.emftext.sdk.codegen.composites.StringComposite sc) {
+	private void addFields(StringComposite sc) {
 		sc.add("private String text;");
 		sc.add("private " + E_OBJECT + " linkTarget;");
 		sc.add("private " + I_REGION + " region;");
