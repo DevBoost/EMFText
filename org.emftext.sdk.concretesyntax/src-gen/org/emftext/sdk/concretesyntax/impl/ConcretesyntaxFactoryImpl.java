f43bec48b61484ea28ff13f2d17d7ff7f745ddc4
Mirko Seifert <mirko.seifert@devboost.de>
2010-02-15 14:39:37 +0000

    removed unused imports

--- org.emftext.sdk.concretesyntax/src-gen/org/emftext/sdk/concretesyntax/impl/ConcretesyntaxFactoryImpl.java
+++ org.emftext.sdk.concretesyntax/src-gen/org/emftext/sdk/concretesyntax/impl/ConcretesyntaxFactoryImpl.java
@@ -19,7 +19,6 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.impl.EFactoryImpl;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
-import org.emftext.sdk.concretesyntax.*;
 import org.emftext.sdk.concretesyntax.Abstract;
 import org.emftext.sdk.concretesyntax.Annotation;
 import org.emftext.sdk.concretesyntax.AnnotationType;
