c15d3580520bcb138b4be8967be6404ce0d6fb49
Mirko Seifert <mirko.seifert@devboost.de>
2010-06-07 19:41:22 +0000

    fixed version constant

--- org.emftext.sdk/src/org/emftext/sdk/EMFTextSDKPlugin.java
+++ org.emftext.sdk/src/org/emftext/sdk/EMFTextSDKPlugin.java
@@ -28,7 +28,7 @@ public class EMFTextSDKPlugin extends Plugin {
 
 	public static final String PLUGIN_ID = "org.emftext.sdk";
 
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.3.0";
 	
 	private static EMFTextSDKPlugin plugin;
 	
