5b0a4891bb8a4d41ece86ae18b96352ed61b00f6
Mirko Seifert <mirko.seifert@devboost.de>
2010-09-06 13:42:19 +0000

    adjusted interface

--- org.emftext.access/src/org/emftext/access/resource/IUIMetaInformation.java
+++ org.emftext.access/src/org/emftext/access/resource/IUIMetaInformation.java
@@ -9,6 +9,18 @@ public interface IUIMetaInformation extends IMetaInformation {
 	 * tokens.
 	 * 
 	 * @return a scanner instance.
+	 * 
+	 * @deprecated this method is only available for plug-ins generated with EMFText 1.3.0 or before
 	 */
 	public org.eclipse.jface.text.rules.ITokenScanner createTokenScanner(IColorManager colorManager);
+
+	/**
+	 * Return a scanner capable to split the underlying text file into JFace
+	 * tokens.
+	 * 
+	 * @return a scanner instance.
+	 * 
+	 * @since EMFText 1.3.1
+	 */
+	public org.eclipse.jface.text.rules.ITokenScanner createTokenScanner(IResource resource, IColorManager colorManager);
 }
