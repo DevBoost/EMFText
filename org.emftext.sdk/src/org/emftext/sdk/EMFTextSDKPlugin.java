5f5264810ed24c6e16b79f93468558fee20cc2af
Mirko Seifert <mirko.seifert@devboost.de>
2010-10-07 13:29:12 +0000

    fixed bug 879: Set SDK plug-in version in EMFTextSDKPlugin before generating languages

--- org.emftext.sdk/src/org/emftext/sdk/EMFTextSDKPlugin.java
+++ org.emftext.sdk/src/org/emftext/sdk/EMFTextSDKPlugin.java
@@ -28,7 +28,7 @@ public class EMFTextSDKPlugin extends Plugin {
 
 	public static final String PLUGIN_ID = "org.emftext.sdk";
 
-	public static final String VERSION = "1.3.2";
+	public static final String VERSION = "1.3.3";
 	
 	private static EMFTextSDKPlugin plugin;
 	
