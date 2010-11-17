c3720a6a2c01f1111f0405d9d6782b40a4484694
Mirko Seifert <mirko.seifert@devboost.de>
2010-11-17 19:36:18 +0000

    regenerated CS with current SDK

--- org.emftext.sdk.concretesyntax.resource.cs/src-gen/org/emftext/sdk/concretesyntax/resource/cs/mopp/CsMarkerHelper.java
+++ org.emftext.sdk.concretesyntax.resource.cs/src-gen/org/emftext/sdk/concretesyntax/resource/cs/mopp/CsMarkerHelper.java
@@ -36,6 +36,47 @@ public class CsMarkerHelper {
 	public static int MAXIMUM_MARKERS = 500;
 	
 	/**
+	 * We use a queue to aggregate commands that create or remove markers. This is
+	 * basically for performance reasons. Without the queue we would need to create a
+	 * job for each marker creation/removal, which creates tons of threads and takes
+	 * very long time.
+	 */
+	private final static MarkerCommandQueue COMMAND_QUEUE = new MarkerCommandQueue();
+	
+	private static class MarkerCommandQueue {
+		
+		private java.util.List<org.emftext.sdk.concretesyntax.resource.cs.ICsCommand<Object>> commands = new java.util.ArrayList<org.emftext.sdk.concretesyntax.resource.cs.ICsCommand<Object>>();
+		
+		public void addCommand(org.emftext.sdk.concretesyntax.resource.cs.ICsCommand<Object> command) {
+			synchronized(commands) {
+				commands.add(command);
+				// we only need to schedule a job, if the queue was empty
+				if (commands.size() == 1) {
+					scheduleRunCommandsJob();
+				}
+			}
+		}
+		
+		private void scheduleRunCommandsJob() {
+			new org.eclipse.core.runtime.jobs.Job("updating markers") {
+				@Override				
+				protected org.eclipse.core.runtime.IStatus run(org.eclipse.core.runtime.IProgressMonitor monitor) {
+					java.util.List<org.emftext.sdk.concretesyntax.resource.cs.ICsCommand<Object>> commandsToProcess = new java.util.ArrayList<org.emftext.sdk.concretesyntax.resource.cs.ICsCommand<Object>>();
+					synchronized(commands) {
+						commandsToProcess.addAll(commands);
+						commands.clear();
+					}
+					for (org.emftext.sdk.concretesyntax.resource.cs.ICsCommand<Object> command : commandsToProcess) {
+						command.execute(null);
+					}
+					return org.eclipse.core.runtime.Status.OK_STATUS;
+				}
+			}.schedule();
+		}
+		
+	}
+	
+	/**
 	 * Marks a file with markers.
 	 * 
 	 * @param resource The resource that is the file to mark.
@@ -54,64 +95,63 @@ public class CsMarkerHelper {
 		if (file == null) {
 			return;
 		}
-		new org.eclipse.core.runtime.jobs.Job("marking") {
-			@Override			
-			protected org.eclipse.core.runtime.IStatus run(org.eclipse.core.runtime.IProgressMonitor monitor) {
-				createMarkerFromDiagnostic(file, diagnostic);
-				return org.eclipse.core.runtime.Status.OK_STATUS;
-			}
-		}.schedule();
+		createMarkerFromDiagnostic(file, diagnostic);
 	}
 	
-	private static void createMarkerFromDiagnostic(org.eclipse.core.resources.IFile file, final org.emftext.sdk.concretesyntax.resource.cs.ICsTextDiagnostic diagnostic) {
-		org.emftext.sdk.concretesyntax.resource.cs.ICsProblem problem = diagnostic.getProblem();
+	private static void createMarkerFromDiagnostic(final org.eclipse.core.resources.IFile file, final org.emftext.sdk.concretesyntax.resource.cs.ICsTextDiagnostic diagnostic) {
+		final org.emftext.sdk.concretesyntax.resource.cs.ICsProblem problem = diagnostic.getProblem();
 		org.emftext.sdk.concretesyntax.resource.cs.CsEProblemType problemType = problem.getType();
-		String markerID = getMarkerID(problemType);
-		try {
-			// if there are too many markers, we do not add new ones
-			if (file.findMarkers(markerID, false, org.eclipse.core.resources.IResource.DEPTH_ZERO).length >= MAXIMUM_MARKERS) {
-				return;
-			}
-			
-			org.eclipse.core.resources.IMarker marker = file.createMarker(markerID);
-			if (problem.getSeverity() == org.emftext.sdk.concretesyntax.resource.cs.CsEProblemSeverity.ERROR) {
-				marker.setAttribute(org.eclipse.core.resources.IMarker.SEVERITY, org.eclipse.core.resources.IMarker.SEVERITY_ERROR);
-			} else {
-				marker.setAttribute(org.eclipse.core.resources.IMarker.SEVERITY, org.eclipse.core.resources.IMarker.SEVERITY_WARNING);
-			}
-			marker.setAttribute(org.eclipse.core.resources.IMarker.MESSAGE, diagnostic.getMessage());
-			org.emftext.sdk.concretesyntax.resource.cs.ICsTextDiagnostic textDiagnostic = (org.emftext.sdk.concretesyntax.resource.cs.ICsTextDiagnostic) diagnostic;
-			marker.setAttribute(org.eclipse.core.resources.IMarker.LINE_NUMBER, textDiagnostic.getLine());
-			marker.setAttribute(org.eclipse.core.resources.IMarker.CHAR_START, textDiagnostic.getCharStart());
-			marker.setAttribute(org.eclipse.core.resources.IMarker.CHAR_END, textDiagnostic.getCharEnd() + 1);
-			if (diagnostic instanceof org.emftext.sdk.concretesyntax.resource.cs.mopp.CsResource.ElementBasedTextDiagnostic) {
-				org.eclipse.emf.ecore.EObject element = ((org.emftext.sdk.concretesyntax.resource.cs.mopp.CsResource.ElementBasedTextDiagnostic) diagnostic).getElement();
-				if (element != null) {
-					org.eclipse.emf.ecore.resource.Resource eResource = element.eResource();
-					if (eResource != null) {
-						marker.setAttribute(org.eclipse.emf.ecore.util.EcoreValidator.URI_ATTRIBUTE, eResource.getURI().toString() + "#" + eResource.getURIFragment(element));
+		final String markerID = getMarkerID(problemType);
+		COMMAND_QUEUE.addCommand(new org.emftext.sdk.concretesyntax.resource.cs.ICsCommand<Object>() {
+			public boolean execute(Object context) {
+				try {
+					// if there are too many markers, we do not add new ones
+					if (file.findMarkers(markerID, false, org.eclipse.core.resources.IResource.DEPTH_ZERO).length >= MAXIMUM_MARKERS) {
+						return true;
+					}
+					
+					org.eclipse.core.resources.IMarker marker = file.createMarker(markerID);
+					if (problem.getSeverity() == org.emftext.sdk.concretesyntax.resource.cs.CsEProblemSeverity.ERROR) {
+						marker.setAttribute(org.eclipse.core.resources.IMarker.SEVERITY, org.eclipse.core.resources.IMarker.SEVERITY_ERROR);
+					} else {
+						marker.setAttribute(org.eclipse.core.resources.IMarker.SEVERITY, org.eclipse.core.resources.IMarker.SEVERITY_WARNING);
+					}
+					marker.setAttribute(org.eclipse.core.resources.IMarker.MESSAGE, diagnostic.getMessage());
+					org.emftext.sdk.concretesyntax.resource.cs.ICsTextDiagnostic textDiagnostic = (org.emftext.sdk.concretesyntax.resource.cs.ICsTextDiagnostic) diagnostic;
+					marker.setAttribute(org.eclipse.core.resources.IMarker.LINE_NUMBER, textDiagnostic.getLine());
+					marker.setAttribute(org.eclipse.core.resources.IMarker.CHAR_START, textDiagnostic.getCharStart());
+					marker.setAttribute(org.eclipse.core.resources.IMarker.CHAR_END, textDiagnostic.getCharEnd() + 1);
+					if (diagnostic instanceof org.emftext.sdk.concretesyntax.resource.cs.mopp.CsResource.ElementBasedTextDiagnostic) {
+						org.eclipse.emf.ecore.EObject element = ((org.emftext.sdk.concretesyntax.resource.cs.mopp.CsResource.ElementBasedTextDiagnostic) diagnostic).getElement();
+						if (element != null) {
+							org.eclipse.emf.ecore.resource.Resource eResource = element.eResource();
+							if (eResource != null) {
+								marker.setAttribute(org.eclipse.emf.ecore.util.EcoreValidator.URI_ATTRIBUTE, eResource.getURI().toString() + "#" + eResource.getURIFragment(element));
+							}
+						}
+					}
+					java.util.Collection<org.emftext.sdk.concretesyntax.resource.cs.ICsQuickFix> quickFixes = textDiagnostic.getProblem().getQuickFixes();
+					java.util.Collection<Object> sourceIDs = new java.util.ArrayList<Object>();
+					if (quickFixes != null) {
+						for (org.emftext.sdk.concretesyntax.resource.cs.ICsQuickFix quickFix : quickFixes) {
+							if (quickFix != null) {
+								sourceIDs.add(quickFix.getContextAsString());
+							}
+						}
+					}
+					if (!sourceIDs.isEmpty()) {
+						marker.setAttribute(org.eclipse.core.resources.IMarker.SOURCE_ID, org.emftext.sdk.concretesyntax.resource.cs.util.CsStringUtil.explode(sourceIDs, "|"));
+					}
+				} catch (org.eclipse.core.runtime.CoreException ce) {
+					if (ce.getMessage().matches("Marker.*not found.")) {
+						// ignore
+					} else {
+						org.emftext.sdk.concretesyntax.resource.cs.mopp.CsPlugin.logError("Error while creating marks for resource:", ce);
 					}
 				}
+				return true;
 			}
-			java.util.Collection<org.emftext.sdk.concretesyntax.resource.cs.ICsQuickFix> quickFixes = textDiagnostic.getProblem().getQuickFixes();
-			java.util.Collection<Object> sourceIDs = new java.util.ArrayList<Object>();
-			if (quickFixes != null) {
-				for (org.emftext.sdk.concretesyntax.resource.cs.ICsQuickFix quickFix : quickFixes) {
-					if (quickFix != null) {
-						sourceIDs.add(quickFix.getContextAsString());
-					}
-				}
-			}
-			if (!sourceIDs.isEmpty()) {
-				marker.setAttribute(org.eclipse.core.resources.IMarker.SOURCE_ID, org.emftext.sdk.concretesyntax.resource.cs.util.CsStringUtil.explode(sourceIDs, "|"));
-			}
-		} catch (org.eclipse.core.runtime.CoreException ce) {
-			if (ce.getMessage().matches("Marker.*not found.")) {
-				// ignore
-			} else {
-				org.emftext.sdk.concretesyntax.resource.cs.mopp.CsPlugin.logError("Error marking resource:", ce);
-			}
-		}
+		});
 	}
 	
 	/**
@@ -132,21 +172,20 @@ public class CsMarkerHelper {
 			return;
 		}
 		final String markerType = getMarkerID(problemType);
-		new org.eclipse.core.runtime.jobs.Job("unmarking") {
-			@Override			
-			protected org.eclipse.core.runtime.IStatus run(org.eclipse.core.runtime.IProgressMonitor monitor) {
+		COMMAND_QUEUE.addCommand(new org.emftext.sdk.concretesyntax.resource.cs.ICsCommand<Object>() {
+			public boolean execute(Object context) {
 				try {
 					file.deleteMarkers(markerType, false, org.eclipse.core.resources.IResource.DEPTH_ZERO);
 				} catch (org.eclipse.core.runtime.CoreException ce) {
 					if (ce.getMessage().matches("Marker.*not found.")) {
 						// ignore
 					} else {
-						org.emftext.sdk.concretesyntax.resource.cs.mopp.CsPlugin.logError("Error marking resource:", ce);
+						org.emftext.sdk.concretesyntax.resource.cs.mopp.CsPlugin.logError("Error while removing markers from resource:", ce);
 					}
 				}
-				return org.eclipse.core.runtime.Status.OK_STATUS;
+				return true;
 			}
-		}.schedule();
+		});
 	}
 	
 	private static String getMarkerID(org.emftext.sdk.concretesyntax.resource.cs.CsEProblemType problemType) {
