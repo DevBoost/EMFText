4e2db318254d5ec92515defc274b8bd2c796864b
Christian Wende <christian.wende@devboost.de>
2011-02-18 15:59:41 +0000

    added quickfix for bug #1676

--- org.emftext.sdk.concretesyntax.resource.cs.post_processing/src/org/emftext/sdk/syntax_analysis/SubclassRestrictionAnalyser.java
+++ org.emftext.sdk.concretesyntax.resource.cs.post_processing/src/org/emftext/sdk/syntax_analysis/SubclassRestrictionAnalyser.java
@@ -17,6 +17,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.codegen.ecore.genmodel.GenClass;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.emftext.sdk.AbstractPostProcessor;
 import org.emftext.sdk.concretesyntax.ConcreteSyntax;
@@ -35,7 +36,9 @@ public class SubclassRestrictionAnalyser extends AbstractPostProcessor {
 		}
 		List<Rule> rules = syntax.getAllRules();
 		for (Rule rule : rules) {
-			Collection<Containment> containments = EObjectUtil.getObjectsByType(rule.eAllContents(), ConcretesyntaxPackage.eINSTANCE.getContainment());
+			Collection<Containment> containments = EObjectUtil
+					.getObjectsByType(rule.eAllContents(),
+							ConcretesyntaxPackage.eINSTANCE.getContainment());
 			for (Containment containment : containments) {
 				boolean hasSyntax = analyseContainment(rules, containment);
 				if (!hasSyntax) {
@@ -62,11 +65,47 @@ public class SubclassRestrictionAnalyser extends AbstractPostProcessor {
 			EClass allowedEType = allowedType.getEcoreClass();
 			for (Rule existingRule : rules) {
 				EClass metaclass = existingRule.getMetaclass().getEcoreClass();
-				if (allowedEType.isSuperTypeOf(metaclass)) {
+				if (isSuperTypeOf(allowedEType, metaclass)) {
 					return true;
 				}
 			}
 		}
 		return false;
+	}
+
+	/**
+	 * TODO this is a quick fix for bug
+	 * http://mantis-st.inf.tu-dresden.de/view.php?id=1676 and should be
+	 * replaced by a real fix
+	 * 
+	 * @param expectedSupertype
+	 *            the expected supertype
+	 * @param subtype
+	 *            the expected subtype
+	 * @return
+	 */
+	private boolean isSuperTypeOf(EClass expectedSupertype, EClass subtype) {
+		// TODO this should work, however fails as reported in bug
+		// http://mantis-st.inf.tu-dresden.de/view.php?id=1676
+		// return allowedEType.isSuperTypeOf(metaclass);
+		//
+		// dirty quickfix below...
+		
+		if (subtype.getName().equals(expectedSupertype.getName())
+				&& subtype.getEPackage().getName()
+						.equals(expectedSupertype.getEPackage().getName())) {
+			return true;
+		}
+		EList<EClass> eAllSuperTypes = subtype.getEAllSuperTypes();
+		for (EClass supertypeCandidate : eAllSuperTypes) {
+			if (supertypeCandidate.getName()
+					.equals(expectedSupertype.getName())
+					&& supertypeCandidate.getEPackage().getName()
+							.equals(expectedSupertype.getEPackage().getName())) {
+				return true;
+			}
+		}
+		return false;
+		
 	}
 }
