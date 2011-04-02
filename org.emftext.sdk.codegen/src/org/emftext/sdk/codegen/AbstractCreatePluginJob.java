fa845ad3a5d733b4eae775b4998ba9c2c22890ed
Mirko Seifert <mirko.seifert@devboost.de>
2011-04-02 16:44:27 +0000

    removed debug output

--- org.emftext.sdk.codegen/src/org/emftext/sdk/codegen/AbstractCreatePluginJob.java
+++ org.emftext.sdk.codegen/src/org/emftext/sdk/codegen/AbstractCreatePluginJob.java
@@ -17,7 +17,6 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Status;
 import org.emftext.sdk.EMFTextSDKPlugin;
 
 public abstract class AbstractCreatePluginJob {
@@ -27,7 +26,6 @@ public abstract class AbstractCreatePluginJob {
 		if (project != null) {
 			project.refreshLocal(IProject.DEPTH_INFINITE, monitor);
 			project.touch(new NullProgressMonitor());
-			EMFTextSDKPlugin.logProblem(Status.INFO, "Project (" + project + ") was refreshed and touched.", null);
 		} else {
 			EMFTextSDKPlugin.logWarning("Project (" + project + ") does not exist, can't refresh.", null);
 		}
