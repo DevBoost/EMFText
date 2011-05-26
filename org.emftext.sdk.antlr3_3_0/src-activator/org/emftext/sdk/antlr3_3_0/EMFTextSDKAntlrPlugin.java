a7d2fa0a6bcadefa767b0b1fc174e31d87db1eeb
Mirko Seifert <mirko.seifert@devboost.de>
2011-05-26 07:19:44 +0000

    updated copyright headers

--- org.emftext.sdk.antlr3_3_0/src-activator/org/emftext/sdk/antlr3_3_0/EMFTextSDKAntlrPlugin.java
+++ org.emftext.sdk.antlr3_3_0/src-activator/org/emftext/sdk/antlr3_3_0/EMFTextSDKAntlrPlugin.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2006-2010 
+ * Copyright (c) 2006-2011
  * Software Technology Group, Dresden University of Technology
- * 
+ *
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0 
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
- *   Software Technology Group - TU Dresden, Germany 
+ *   Software Technology Group - TU Dresden, Germany
  *      - initial API and implementation
  ******************************************************************************/
 package org.emftext.sdk.antlr3_3_0;
@@ -19,9 +19,9 @@ import org.osgi.framework.BundleContext;
 public class EMFTextSDKAntlrPlugin extends Plugin {
 
 	public static final String PLUGIN_ID = "org.emftext.sdk.antlr3_3_0";
-	
+
 	private static EMFTextSDKAntlrPlugin plugin;
-	
+
 	/**
 	 * The constructor
 	 */
