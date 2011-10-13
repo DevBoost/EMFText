38adee1bceff294f6dd71a828f1c44e5682bcaf9
Mirko Seifert <mirko.seifert@devboost.de>
2011-10-13 19:43:48 +0000

    added missing documentation for new override option

--- org.emftext.sdk.concretesyntax/src-gen/org/emftext/sdk/concretesyntax/impl/ConcretesyntaxPackageImpl.java
+++ org.emftext.sdk.concretesyntax/src-gen/org/emftext/sdk/concretesyntax/impl/ConcretesyntaxPackageImpl.java
@@ -4641,6 +4641,12 @@ public class ConcretesyntaxPackageImpl extends EPackageImpl implements Concretes
 			 "documentation", "If set to <code>false</code>, the ExpectationConstants class will not be overridden. The default value for this option is <code>true</code>."
 		   });		
 		addAnnotation
+		  (optionTypesEEnum.getELiterals().get(273), 
+		   source, 
+		   new String[] {
+			 "documentation", "If set to <code>false</code>, the EclipseProxy class will not be overridden. The default value for this option is <code>true</code>."
+		   });		
+		addAnnotation
 		  (tokenStyleEClass, 
 		   source, 
 		   new String[] {
