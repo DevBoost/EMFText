88e893829069a77c3002b305d3e239ffc2bb6819
Mirko Seifert <mirko.seifert@devboost.de>
2012-01-27 21:31:07 +0000

    updated license header

--- org.emftext.sdk.concretesyntax.resource.cs/src-gen/org/emftext/sdk/concretesyntax/resource/cs/mopp/CsSyntaxCoverageInformationProvider.java
+++ org.emftext.sdk.concretesyntax.resource.cs/src-gen/org/emftext/sdk/concretesyntax/resource/cs/mopp/CsSyntaxCoverageInformationProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2011
+ * Copyright (c) 2006-2012
  * Software Technology Group, Dresden University of Technology
  * 
  * All rights reserved. This program and the accompanying materials
