84d8df92decb6d7bf01c5110043fbdb1f3a9956b
Mirko Seifert <mirko.seifert@devboost.de>
2012-03-07 19:32:36 +0000

    fixed bug 1418: Tag collect-in tokens as deprecated

--- org.emftext.sdk.concretesyntax.resource.cs/src/org/emftext/sdk/concretesyntax/resource/cs/postprocessing/syntax_analysis/CollectInTokenAnalyser.java
+++ org.emftext.sdk.concretesyntax.resource.cs/src/org/emftext/sdk/concretesyntax/resource/cs/postprocessing/syntax_analysis/CollectInTokenAnalyser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2011
+ * Copyright (c) 2006-2012
  * Software Technology Group, Dresden University of Technology
  * 
  * All rights reserved. This program and the accompanying materials
@@ -14,6 +14,7 @@
 package org.emftext.sdk.concretesyntax.resource.cs.postprocessing.syntax_analysis;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.emftext.sdk.concretesyntax.CompleteTokenDefinition;
 import org.emftext.sdk.concretesyntax.ConcreteSyntax;
@@ -21,6 +22,7 @@ import org.emftext.sdk.concretesyntax.ConcretesyntaxPackage;
 import org.emftext.sdk.concretesyntax.Placeholder;
 import org.emftext.sdk.concretesyntax.ReferencableTokenDefinition;
 import org.emftext.sdk.concretesyntax.Rule;
+import org.emftext.sdk.concretesyntax.TokenDirective;
 import org.emftext.sdk.concretesyntax.resource.cs.mopp.CsAnalysisProblemType;
 import org.emftext.sdk.concretesyntax.resource.cs.postprocessing.AbstractPostProcessor;
 import org.emftext.sdk.concretesyntax.resource.cs.util.CsEObjectUtil;
@@ -28,10 +30,33 @@ import org.emftext.sdk.concretesyntax.resource.cs.util.CsEObjectUtil;
 public class CollectInTokenAnalyser extends AbstractPostProcessor {
 
 	private static final String COLLECT_IN_TOKEN_USED_IN_RULE_WARNING = 
-		"Token %s will never be matched here, since it is a collect-in token.";
-	
+			"Token %s will never be matched here, since it is a collect-in token.";
+		
+	private static final String COLLECT_IN_TOKEN_DEPRECATED_WARNING = 
+			"Collect-in tokens are deprecated as of version 1.4.1 of EMFText. Use the commons.layout plug-in to capture layout in model instances instead.";
+		
 	@Override
 	public void analyse(ConcreteSyntax syntax) {
+		tagCollectInTokensAsDeprecated(syntax);
+		checkCollectInTokensUsedInSyntaxRules(syntax);
+	}
+
+	private void tagCollectInTokensAsDeprecated(ConcreteSyntax syntax) {
+		List<TokenDirective> tokens = syntax.getTokens();
+		for (TokenDirective tokenDirective : tokens) {
+			if (tokenDirective instanceof CompleteTokenDefinition) {
+				CompleteTokenDefinition completeTokenDefinition = (CompleteTokenDefinition) tokenDirective;
+				if (isCollectInToken(completeTokenDefinition)) {
+					addProblem(
+							CsAnalysisProblemType.COLLECT_IN_TOKEN_USED_IN_RULE,
+							COLLECT_IN_TOKEN_DEPRECATED_WARNING,
+							completeTokenDefinition);
+				}
+			}
+		}
+	}
+
+	private void checkCollectInTokensUsedInSyntaxRules(ConcreteSyntax syntax) {
 		for (Rule rule : syntax.getAllRules()) {
 			Collection<Placeholder> placeholders = CsEObjectUtil.getObjectsByType(rule.eAllContents(), ConcretesyntaxPackage.eINSTANCE.getPlaceholder());
 			for (Placeholder placeholder : placeholders) {
@@ -41,7 +66,7 @@ public class CollectInTokenAnalyser extends AbstractPostProcessor {
 				}
 				if (token instanceof CompleteTokenDefinition) {
 					CompleteTokenDefinition completeDefinition = (CompleteTokenDefinition) token;
-					if (completeDefinition.getAttributeName() != null) {
+					if (isCollectInToken(completeDefinition)) {
 						addProblem(
 								CsAnalysisProblemType.COLLECT_IN_TOKEN_USED_IN_RULE,
 								String.format(COLLECT_IN_TOKEN_USED_IN_RULE_WARNING, token.getName()),
@@ -52,4 +77,7 @@ public class CollectInTokenAnalyser extends AbstractPostProcessor {
 		}
 	}
 
+	private boolean isCollectInToken(CompleteTokenDefinition completeDefinition) {
+		return completeDefinition.getAttributeName() != null;
+	}
 }
