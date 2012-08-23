697d5734c6cdaa7d8fe7a191a6ce962c751b7f8d
Jendrik Johannes <jendrik.johannes@devboost.de>
2012-08-23 17:24:15 +0200

    cleanup and re-generated with current EMFText SDK

--- Core/Commons/org.emftext.commons.jdt.edit/src/org/emftext/commons/jdt/provider/JDTJavaClassifierItemProvider.java
+++ Core/Commons/org.emftext.commons.jdt.edit/src/org/emftext/commons/jdt/provider/JDTJavaClassifierItemProvider.java
@@ -59,132 +59,133 @@ public class JDTJavaClassifierItemProvider
 		IItemLabelProvider,
 		IItemPropertySource {
 	/**
-	 * This constructs an instance from a factory and a notifier.
-	 * <!-- begin-user-doc -->
+   * This constructs an instance from a factory and a notifier.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+   * @generated
+   */
 	public JDTJavaClassifierItemProvider(AdapterFactory adapterFactory) {
-		super(adapterFactory);
-	}
+    super(adapterFactory);
+  }
 
 	/**
-	 * This returns the property descriptors for the adapted class.
-	 * <!-- begin-user-doc -->
+   * This returns the property descriptors for the adapted class.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+   * @generated
+   */
 	@Override
 	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
-		if (itemPropertyDescriptors == null) {
-			super.getPropertyDescriptors(object);
+    if (itemPropertyDescriptors == null)
+    {
+      super.getPropertyDescriptors(object);
 
-			addSimpleNamePropertyDescriptor(object);
-			addPackageNamePropertyDescriptor(object);
-			addEnclosingTypeNamesPropertyDescriptor(object);
-			addPathPropertyDescriptor(object);
-		}
-		return itemPropertyDescriptors;
-	}
+      addSimpleNamePropertyDescriptor(object);
+      addPackageNamePropertyDescriptor(object);
+      addEnclosingTypeNamesPropertyDescriptor(object);
+      addPathPropertyDescriptor(object);
+    }
+    return itemPropertyDescriptors;
+  }
 
 	/**
-	 * This adds a property descriptor for the Simple Name feature.
-	 * <!-- begin-user-doc -->
+   * This adds a property descriptor for the Simple Name feature.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+   * @generated
+   */
 	protected void addSimpleNamePropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_JDTJavaClassifier_simpleName_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_JDTJavaClassifier_simpleName_feature", "_UI_JDTJavaClassifier_type"),
-				 JdtPackage.Literals.JDT_JAVA_CLASSIFIER__SIMPLE_NAME,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 null,
-				 null));
-	}
+    itemPropertyDescriptors.add
+      (createItemPropertyDescriptor
+        (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+         getResourceLocator(),
+         getString("_UI_JDTJavaClassifier_simpleName_feature"),
+         getString("_UI_PropertyDescriptor_description", "_UI_JDTJavaClassifier_simpleName_feature", "_UI_JDTJavaClassifier_type"),
+         JdtPackage.Literals.JDT_JAVA_CLASSIFIER__SIMPLE_NAME,
+         true,
+         false,
+         false,
+         ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+         null,
+         null));
+  }
 
 	/**
-	 * This adds a property descriptor for the Package Name feature.
-	 * <!-- begin-user-doc -->
+   * This adds a property descriptor for the Package Name feature.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+   * @generated
+   */
 	protected void addPackageNamePropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_JDTJavaClassifier_packageName_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_JDTJavaClassifier_packageName_feature", "_UI_JDTJavaClassifier_type"),
-				 JdtPackage.Literals.JDT_JAVA_CLASSIFIER__PACKAGE_NAME,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 null,
-				 null));
-	}
+    itemPropertyDescriptors.add
+      (createItemPropertyDescriptor
+        (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+         getResourceLocator(),
+         getString("_UI_JDTJavaClassifier_packageName_feature"),
+         getString("_UI_PropertyDescriptor_description", "_UI_JDTJavaClassifier_packageName_feature", "_UI_JDTJavaClassifier_type"),
+         JdtPackage.Literals.JDT_JAVA_CLASSIFIER__PACKAGE_NAME,
+         true,
+         false,
+         false,
+         ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+         null,
+         null));
+  }
 
 	/**
-	 * This adds a property descriptor for the Enclosing Type Names feature.
-	 * <!-- begin-user-doc -->
+   * This adds a property descriptor for the Enclosing Type Names feature.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+   * @generated
+   */
 	protected void addEnclosingTypeNamesPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_JDTJavaClassifier_enclosingTypeNames_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_JDTJavaClassifier_enclosingTypeNames_feature", "_UI_JDTJavaClassifier_type"),
-				 JdtPackage.Literals.JDT_JAVA_CLASSIFIER__ENCLOSING_TYPE_NAMES,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 null,
-				 null));
-	}
+    itemPropertyDescriptors.add
+      (createItemPropertyDescriptor
+        (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+         getResourceLocator(),
+         getString("_UI_JDTJavaClassifier_enclosingTypeNames_feature"),
+         getString("_UI_PropertyDescriptor_description", "_UI_JDTJavaClassifier_enclosingTypeNames_feature", "_UI_JDTJavaClassifier_type"),
+         JdtPackage.Literals.JDT_JAVA_CLASSIFIER__ENCLOSING_TYPE_NAMES,
+         true,
+         false,
+         false,
+         ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+         null,
+         null));
+  }
 
 	/**
-	 * This adds a property descriptor for the Path feature.
-	 * <!-- begin-user-doc -->
+   * This adds a property descriptor for the Path feature.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+   * @generated
+   */
 	protected void addPathPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_JDTJavaClassifier_path_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_JDTJavaClassifier_path_feature", "_UI_JDTJavaClassifier_type"),
-				 JdtPackage.Literals.JDT_JAVA_CLASSIFIER__PATH,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 null,
-				 null));
-	}
+    itemPropertyDescriptors.add
+      (createItemPropertyDescriptor
+        (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+         getResourceLocator(),
+         getString("_UI_JDTJavaClassifier_path_feature"),
+         getString("_UI_PropertyDescriptor_description", "_UI_JDTJavaClassifier_path_feature", "_UI_JDTJavaClassifier_type"),
+         JdtPackage.Literals.JDT_JAVA_CLASSIFIER__PATH,
+         true,
+         false,
+         false,
+         ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+         null,
+         null));
+  }
 
 	/**
-	 * This returns JDTJavaClassifier.gif.
-	 * <!-- begin-user-doc -->
+   * This returns JDTJavaClassifier.gif.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+   * @generated
+   */
 	@Override
 	public Object getImage(Object object) {
-		return overlayImage(object, getResourceLocator().getImage("full/obj16/JDTJavaClassifier"));
-	}
+    return overlayImage(object, getResourceLocator().getImage("full/obj16/JDTJavaClassifier"));
+  }
 
 	/**
 	 * This returns the label text for the adapted class.
@@ -201,48 +202,49 @@ public class JDTJavaClassifierItemProvider
 	}
 
 	/**
-	 * This handles model notifications by calling {@link #updateChildren} to update any cached
-	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-	 * <!-- begin-user-doc -->
+   * This handles model notifications by calling {@link #updateChildren} to update any cached
+   * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+   * @generated
+   */
 	@Override
 	public void notifyChanged(Notification notification) {
-		updateChildren(notification);
+    updateChildren(notification);
 
-		switch (notification.getFeatureID(JDTJavaClassifier.class)) {
-			case JdtPackage.JDT_JAVA_CLASSIFIER__SIMPLE_NAME:
-			case JdtPackage.JDT_JAVA_CLASSIFIER__PACKAGE_NAME:
-			case JdtPackage.JDT_JAVA_CLASSIFIER__ENCLOSING_TYPE_NAMES:
-			case JdtPackage.JDT_JAVA_CLASSIFIER__PATH:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
-				return;
-		}
-		super.notifyChanged(notification);
-	}
+    switch (notification.getFeatureID(JDTJavaClassifier.class))
+    {
+      case JdtPackage.JDT_JAVA_CLASSIFIER__SIMPLE_NAME:
+      case JdtPackage.JDT_JAVA_CLASSIFIER__PACKAGE_NAME:
+      case JdtPackage.JDT_JAVA_CLASSIFIER__ENCLOSING_TYPE_NAMES:
+      case JdtPackage.JDT_JAVA_CLASSIFIER__PATH:
+        fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+        return;
+    }
+    super.notifyChanged(notification);
+  }
 
 	/**
-	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-	 * that can be created under this object.
-	 * <!-- begin-user-doc -->
+   * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+   * that can be created under this object.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+   * @generated
+   */
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
-		super.collectNewChildDescriptors(newChildDescriptors, object);
-	}
+    super.collectNewChildDescriptors(newChildDescriptors, object);
+  }
 
 	/**
-	 * Return the resource locator for this item provider's resources.
-	 * <!-- begin-user-doc -->
+   * Return the resource locator for this item provider's resources.
+   * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+   * @generated
+   */
 	@Override
 	public ResourceLocator getResourceLocator() {
-		return JdtEditPlugin.INSTANCE;
-	}
+    return JdtEditPlugin.INSTANCE;
+  }
 
 }
--- Core/Tests/org.emftext.test.atl/model/example.cs
+++
@@ -1,9 +0,0 @@
- SYNTAXDEF example.ecore
-FOR <http://www.eclipse.org/emf/2002/Ecore>
-START EPackage
-
-RULES{
-	
-	EPackage::= "annotate"  ;
-	
-}
