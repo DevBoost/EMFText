184ca7b08e8c456651f63cea2c6bad58d1ee900d
Mirko Seifert <mirko.seifert@devboost.de>
2013-03-29 10:36:38 +0100

    added more invalid generator models that must be ignored refactored code

--- Core/SDK/org.emftext.sdk/src/org/emftext/sdk/finders/GenPackageInRegistryFinder.java
+++ Core/SDK/org.emftext.sdk/src/org/emftext/sdk/finders/GenPackageInRegistryFinder.java
@@ -18,12 +18,12 @@ package org.emftext.sdk.finders;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
 import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
@@ -46,89 +46,112 @@ public class GenPackageInRegistryFinder implements IGenPackageFinder {
 	private static void init() {
 		synchronized (GenPackageInRegistryFinder.class) {
 			if (!isInitialized) {
-				//search all registered generator models
-				final Map<String, URI> packageNsURIToGenModelLocationMap = EcorePlugin.getEPackageNsURIToGenModelLocationMap();
-				for (String nextNS : packageNsURIToGenModelLocationMap.keySet()) {
-					URI genModelURI = packageNsURIToGenModelLocationMap.get(nextNS);
-			    	try {
-			    		final ResourceSet rs = new ResourceSetImpl();
-		        		Resource genModelResource = rs.getResource(genModelURI, true);
-		        		if (genModelResource == null) {
-		        			continue;
-		        		}
-		            	final EList<EObject> contents = genModelResource.getContents();
-		            	if (contents == null || contents.size() == 0) {
-		            		continue;
-		            	}
-						GenModel genModel = (GenModel) contents.get(0);
-		            	for (GenPackage genPackage : genModel.getGenPackages()) {
-		        			if (genPackage != null && !genPackage.eIsProxy()) {
-			            		String nsURI = genPackage.getNSURI();
-			            		final GenPackageInRegistry result = new GenPackageInRegistry(genPackage);
-								cache.put(nsURI, result);
-								registerSubGenPackages(genPackage);
-		        			}
-		            	}
-			    	} catch (Exception e) {
-			    		String uriString = genModelURI.toString();
-			    		// ignore FileNotFoundException caused by the org.eclipse.m2m.qvt.oml plug-in
-			    		// this plug-in does not contain the generator models it registers
-			    		// this is a workaround for Eclipse Bug 288208
-			    		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=288208
-			    		//
-			    		// do also ignore FileNotFoundException caused by some ATL plug-ins
-			    		// this is a workaround for Eclipse Bug 315376
-			    		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=315376
-			    		//
-			    		// do also ignore FileNotFoundException caused by some xText plug-ins
-			    		// this is a workaround for Eclipse Bug 315986
-			    		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=315986
-			    		//
-			    		// do also ignore FileNotFoundException caused by CDO
-			    		// this is a workaround for Eclipse Bug 317821
-			    		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=317821
-			    		//
-			    		// do also ignore FileNotFoundException caused by MOFScript
-			    		// This is a workaround for Eclipse Bug 322642
-			    		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=322642
-			    		//
-			    		// do also ignore FileNotFoundException caused by OCL Tools
-			    		// This is a workaround for Eclipse Bug 322886
-			    		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=322886
-			    		//
-			    		// do also ignore FileNotFoundException caused by some pure::variants plug-ins
-			    		//
-			    		// do also ignore FileNotFoundException caused by Acceleo plug-ins
-			    		// This is a workaround for Eclipse Bug 350348
-			    		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=350348
-						if (!uriString.startsWith("platform:/plugin/com.ps.consul.eclipse.ecore/src/pvmeta.genmodel") &&
-							!uriString.startsWith("platform:/plugin/com.ps.consul.eclipse.ecore/src/pvmodel.genmodel") &&
-							!uriString.startsWith("platform:/plugin/org.eclipse.emf.cdo/model/resource.genmodel") &&
-							!uriString.startsWith("platform:/plugin/org.eclipse.emf.mwe2.language/model/Mwe2.genmodel") &&
-							!uriString.startsWith("platform:/plugin/org.eclipse.m2m.qvt.oml") &&
-							!uriString.startsWith("platform:/plugin/org.eclipse.m2m.atl.profiler.exportmodel/model/exportmodel.genmodel") &&
-							!uriString.startsWith("platform:/plugin/org.eclipse.m2m.atl.profiler.model/model/ATL-Profiler.genmodel") &&
-							!uriString.startsWith("platform:/plugin/org.eclipse.mofscript.model/src/model/mofscriptmodel.genmodel") &&
-							!uriString.startsWith("platform:/plugin/org.eclipse.ocl.examples.xtext.completeocl/org.eclipse.ocl.examples.xtext.completeocl/model/CompleteOCLCST.genmodel") &&
-							!uriString.startsWith("platform:/plugin/org.eclipse.xtext/model/xtext.genmodel") &&
-							!uriString.startsWith("platform:/plugin/org.eclipse.xtext.builder/model/BuilderState.genmodel") &&
-							!uriString.startsWith("platform:/plugin/org.eclipse.xtext.xbase/org/eclipse/xtext/xbase/Xbase.genmodel") &&
-							!uriString.startsWith("platform:/plugin/org.eclipse.acceleo.ide.ui/model/AcceleoWizardModel.genmodel")
-							) {
-				    		EMFTextSDKPlugin.logWarning("Exception while looking up generator model (" + nextNS + ") at " + uriString + " in the registry.", e);
-			    		}
-			    	}
-		        }
+				doInitialize();
 				isInitialized = true;
 			}
 		}
+	}
+
+	private static void doInitialize() {
+		//search all registered generator models
+		Map<String, URI> packageNsURIToGenModelLocationMap = EcorePlugin.getEPackageNsURIToGenModelLocationMap();
+		for (String nextNS : packageNsURIToGenModelLocationMap.keySet()) {
+			URI genModelURI = packageNsURIToGenModelLocationMap.get(nextNS);
+			try {
+				ResourceSet rs = new ResourceSetImpl();
+				Resource genModelResource = rs.getResource(genModelURI, true);
+				if (genModelResource == null) {
+					continue;
+				}
+		    	List<EObject> contents = genModelResource.getContents();
+		    	if (contents == null || contents.size() == 0) {
+		    		continue;
+		    	}
+				GenModel genModel = (GenModel) contents.get(0);
+		    	for (GenPackage genPackage : genModel.getGenPackages()) {
+					if (genPackage != null && !genPackage.eIsProxy()) {
+		        		String nsURI = genPackage.getNSURI();
+		        		GenPackageInRegistry result = new GenPackageInRegistry(genPackage);
+						cache.put(nsURI, result);
+						registerSubGenPackages(genPackage);
+					}
+		    	}
+			} catch (Exception e) {
+				String uriString = genModelURI.toString();
+				if (notIgnorable(uriString)) {
+		    		EMFTextSDKPlugin.logWarning("Exception while looking up generator model (" + nextNS + ") at " + uriString + " in the registry.", e);
+				}
+			}
+		}
+	}
+
+	private static boolean notIgnorable(String uriString) {
+		// ignore FileNotFoundException caused by the org.eclipse.m2m.qvt.oml plug-in
+		// this plug-in does not contain the generator models it registers
+		// this is a workaround for Eclipse Bug 288208
+		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=288208
+		//
+		// do also ignore FileNotFoundException caused by some ATL plug-ins
+		// this is a workaround for Eclipse Bug 315376
+		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=315376
+		//
+		// do also ignore FileNotFoundException caused by some xText plug-ins
+		// this is a workaround for Eclipse Bug 315986
+		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=315986
+		//
+		// do also ignore FileNotFoundException caused by CDO
+		// this is a workaround for Eclipse Bug 317821
+		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=317821
+		//
+		// do also ignore FileNotFoundException caused by MOFScript
+		// This is a workaround for Eclipse Bug 322642
+		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=322642
+		//
+		// do also ignore FileNotFoundException caused by OCL Tools
+		// This is a workaround for Eclipse Bug 322886
+		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=322886
+		//
+		// do also ignore FileNotFoundException caused by some pure::variants plug-ins
+		String[] ignorableURIs = new String[] {
+				"platform:/plugin/com.ps.consul.eclipse.ecore/src/pvmeta.genmodel",
+				"platform:/plugin/com.ps.consul.eclipse.ecore/src/pvmodel.genmodel",
+				"platform:/plugin/org.eclipse.emf.cdo/model/resource.genmodel",
+				"platform:/plugin/org.eclipse.emf.mwe2.language/model/Mwe2.genmodel",
+				"platform:/plugin/org.eclipse.m2m.qvt.oml",
+				"platform:/plugin/org.eclipse.m2m.atl.profiler.exportmodel/model/exportmodel.genmodel",
+				"platform:/plugin/org.eclipse.m2m.atl.profiler.model/model/ATL-Profiler.genmodel",
+				"platform:/plugin/org.eclipse.mofscript.model/src/model/mofscriptmodel.genmodel",
+				"platform:/plugin/org.eclipse.ocl.examples.xtext.completeocl/org.eclipse.ocl.examples.xtext.completeocl/model/CompleteOCLCST.genmodel",
+				"platform:/plugin/org.eclipse.xtext/model/xtext.genmodel",
+				"platform:/plugin/org.eclipse.xtext.builder/model/BuilderState.genmodel",
+				"platform:/plugin/org.eclipse.xtext.xbase/org/eclipse/xtext/xbase/Xbase.genmodel",
+				// do also ignore FileNotFoundException caused by Acceleo plug-ins
+				// This is a workaround for Eclipse Bug 350348
+				// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=350348
+				"platform:/plugin/org.eclipse.acceleo.ide.ui/model/AcceleoWizardModel.genmodel",
+
+				// do ignore some other problem in generator models. these
+				// problem were not reported because the project maintainers
+				// do not seem to care about generator model problem.
+				"platform:/plugin/org.eclipse.ocl/model/OCL.genmodel",
+				"platform:/plugin/org.eclipse.ocl/model/OCLCST.genmodel",
+				"platform:/plugin/org.eclipse.m2e.model.edit/src/main/xsd/org/apache/maven/pom/pom.genmodel",
+		};
+		
+		boolean notIgnorable = true;
+		for (String ignoredURI : ignorableURIs) {
+			boolean ignorable = uriString.startsWith(ignoredURI);
+			notIgnorable &= !ignorable;
+		}
+		
+		return notIgnorable;
 	}
 	
 	private static void registerSubGenPackages(GenPackage parentPackage) {
 		for(GenPackage genPackage : parentPackage.getSubGenPackages()) {
 			if (genPackage != null && !genPackage.eIsProxy()) {
         		String nsURI = genPackage.getNSURI();
-        		final GenPackageInRegistry result = new GenPackageInRegistry(genPackage);
+        		GenPackageInRegistry result = new GenPackageInRegistry(genPackage);
 				cache.put(nsURI, result);
 				registerSubGenPackages(genPackage);
 			}
@@ -136,8 +159,8 @@ public class GenPackageInRegistryFinder implements IGenPackageFinder {
 	}
 	
 	/**
-	 * An implementation of the IResolvedGenPackage that is used to
-	 * return generator packages found in the EMF registry.
+	 * An implementation of the IResolvedGenPackage that is used to return
+	 * generator packages found in the EMF registry.
 	 */
 	private static class GenPackageInRegistry implements IResolvedGenPackage {
 
