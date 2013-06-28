afbcdaf86b710072a7825d03835c42712fe3ebc0
Mirko Seifert <mirko.seifert@devboost.de>
2013-06-28 13:29:15 +0200

    adjusted tests to compile against Kepler

--- Core/Tests/org.emftext.test/src/org/emftext/test/resource/DeterministicLoadTest.java
+++ Core/Tests/org.emftext.test/src/org/emftext/test/resource/DeterministicLoadTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2012
+ * Copyright (c) 2006-2013
  * Software Technology Group, Dresden University of Technology
  * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
  * 
@@ -24,11 +24,18 @@ import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.compare.diff.metamodel.DiffGroup;
-import org.eclipse.emf.compare.diff.metamodel.DiffModel;
-import org.eclipse.emf.compare.diff.service.DiffService;
-import org.eclipse.emf.compare.match.metamodel.MatchModel;
-import org.eclipse.emf.compare.match.service.MatchService;
+import org.eclipse.emf.compare.Comparison;
+import org.eclipse.emf.compare.EMFCompare;
+import org.eclipse.emf.compare.match.DefaultComparisonFactory;
+import org.eclipse.emf.compare.match.DefaultEqualityHelperFactory;
+import org.eclipse.emf.compare.match.DefaultMatchEngine;
+import org.eclipse.emf.compare.match.IComparisonFactory;
+import org.eclipse.emf.compare.match.IMatchEngine;
+import org.eclipse.emf.compare.match.eobject.IEObjectMatcher;
+import org.eclipse.emf.compare.match.impl.MatchEngineFactoryImpl;
+import org.eclipse.emf.compare.match.impl.MatchEngineFactoryRegistryImpl;
+import org.eclipse.emf.compare.scope.IComparisonScope;
+import org.eclipse.emf.compare.utils.UseIdentifiers;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -67,28 +74,34 @@ public class DeterministicLoadTest {
 		final EObject modelObject1 = loadModel(csUri);
 		final EObject modelObject2 = loadModel(csUri);
 
-		compareModels(modelObject1, modelObject2, "testDeterministicLoad");
+		compareModels(modelObject1, modelObject2);
 	}
 
-	private void compareModels(final EObject modelLeft,
-			final EObject modelRight, String testName) throws Exception {
-		final MatchModel inputMatch = MatchService.doMatch(modelLeft,
-				modelRight, null);
-		final DiffModel inputDiff = DiffService.doDiff(inputMatch);
-
-		if (((DiffGroup) inputDiff.getOwnedElements().get(0)).getSubchanges() != 0) {
-			// TODO make this code run against Eclipse Galileo
-			/*
-			final ModelInputSnapshot snapshot = DiffFactory.eINSTANCE
-					.createModelInputSnapshot();
-			snapshot.setDiff(inputDiff);
-			snapshot.setMatch(inputMatch);
-			saveModel(snapshot, testName);
-			*/
+	private void compareModels(EObject modelLeft, EObject modelRight) throws Exception {
+		
+		Comparison comparison = compare(modelLeft, modelRight);
+		
+		if (!comparison.getDifferences().isEmpty()) {
 			fail("Diff failed");
 		}
 	}
 
+	public Comparison compare(EObject modelLeft, EObject modelRight) {
+
+		// Configure EMF Compare
+		IEObjectMatcher matcher = DefaultMatchEngine.createDefaultEObjectMatcher(UseIdentifiers.NEVER);
+		IComparisonFactory comparisonFactory = new DefaultComparisonFactory(new DefaultEqualityHelperFactory());
+		IMatchEngine.Factory matchEngineFactory = new MatchEngineFactoryImpl(matcher, comparisonFactory);
+        matchEngineFactory.setRanking(20);
+        IMatchEngine.Factory.Registry matchEngineRegistry = new MatchEngineFactoryRegistryImpl();
+        matchEngineRegistry.add(matchEngineFactory);
+		EMFCompare comparator = EMFCompare.builder().setMatchEngineFactoryRegistry(matchEngineRegistry).build();
+	 
+		// Compare the two models
+		IComparisonScope scope = EMFCompare.createDefaultScope(modelLeft, modelRight);
+		return comparator.compare(scope);
+	}
+	
 	private void createOutputProject(String projectName, String folderName)
 			throws Exception {
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
