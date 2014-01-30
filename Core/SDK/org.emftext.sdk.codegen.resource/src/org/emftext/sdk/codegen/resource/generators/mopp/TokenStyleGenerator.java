a7f96d656beb28ec7230040126bf13deca124d23
Mirko Seifert <mirko.seifert@devboost.de>
2014-01-30 20:33:30 +0100

    Extended TokenStyleGenerator to generate equals() and hashCode() which will be used to store TokenStyle objects in HashSets and HashMaps

--- Core/SDK/org.emftext.sdk.codegen.resource/src/org/emftext/sdk/codegen/resource/generators/mopp/TokenStyleGenerator.java
+++ Core/SDK/org.emftext.sdk.codegen.resource/src/org/emftext/sdk/codegen/resource/generators/mopp/TokenStyleGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2012
+ * Copyright (c) 2006-2013
  * Software Technology Group, Dresden University of Technology
  * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
  * 
@@ -15,6 +15,8 @@
  ******************************************************************************/
 package org.emftext.sdk.codegen.resource.generators.mopp;
 
+import java.util.Arrays;
+
 import org.emftext.sdk.codegen.parameters.ArtifactParameter;
 import org.emftext.sdk.codegen.resource.GenerationContext;
 import org.emftext.sdk.codegen.resource.generators.JavaBaseGenerator;
@@ -25,7 +27,9 @@ public class TokenStyleGenerator extends JavaBaseGenerator<ArtifactParameter<Gen
 
 	@Override
 	public void generateJavaContents(JavaComposite sc) {
-		sc.add("package " + getResourcePackageName() + ";");sc.addLineBreak();sc.addImportsPlaceholder();
+		sc.add("package " + getResourcePackageName() + ";");
+		sc.addLineBreak();
+		sc.addImportsPlaceholder();
 		sc.addLineBreak();
 		sc.add("public class " + getResourceClassName() + " implements " + iTokenStyleClassName + " {");
 		sc.addLineBreak();
@@ -33,7 +37,10 @@ public class TokenStyleGenerator extends JavaBaseGenerator<ArtifactParameter<Gen
 		addConstructor1(sc);
 		addConstructor2(sc);
         sc.addGettersSetters();
-		sc.add("}");
+        addHashCodeMethod(sc);
+        addEqualsMethod(sc);
+
+        sc.add("}");
 	}
 
 	private void addFields(JavaComposite sc) {
@@ -62,6 +69,49 @@ public class TokenStyleGenerator extends JavaBaseGenerator<ArtifactParameter<Gen
 	private void addConstructor2(JavaComposite sc) {
 		sc.add("public " + getResourceClassName() + "(" + iTokenStyleClassName + " styleToCopy) {");
         sc.add("this(styleToCopy.getColorAsRGB(), styleToCopy.getBackgroundColorAsRGB(), styleToCopy.isBold(), styleToCopy.isItalic(), styleToCopy.isStrikethrough(), styleToCopy.isUnderline());");
+        sc.add("}");
+        sc.addLineBreak();
+	}
+
+	private void addHashCodeMethod(JavaComposite sc) {
+		sc.add("@Override");
+        sc.add("public int hashCode() {");
+        sc.add("final int prime = 31;");
+        sc.add("int result = 1;");
+        sc.add("result = prime * result + " + sc.getClassName(Arrays.class) + ".hashCode(backgroundColorAsRGB);");
+        sc.add("result = prime * result + (bold ? 1231 : 1237);");
+        sc.add("result = prime * result + " + sc.getClassName(Arrays.class) + ".hashCode(colorAsRGB);");
+        sc.add("result = prime * result + (italic ? 1231 : 1237);");
+        sc.add("result = prime * result + (strikethrough ? 1231 : 1237);");
+        sc.add("result = prime * result + (underline ? 1231 : 1237);");
+        sc.add("return result;");
+        sc.add("}");
+        sc.addLineBreak();
+	}
+
+	private void addEqualsMethod(JavaComposite sc) {
+		sc.add("@Override");
+        sc.add("public boolean equals(Object obj) {");
+        sc.add("if (this == obj)");
+        sc.add("return true;");
+        sc.add("if (obj == null)");
+        sc.add("return false;");
+        sc.add("if (getClass() != obj.getClass())");
+        sc.add("return false;");
+        sc.add(tokenStyleClassName + " other = (" + tokenStyleClassName + ") obj;");
+        sc.add("if (!" + sc.getClassName(Arrays.class) + ".equals(backgroundColorAsRGB, other.backgroundColorAsRGB))");
+        sc.add("return false;");
+        sc.add("if (bold != other.bold)");
+        sc.add("return false;");
+        sc.add("if (!" + sc.getClassName(Arrays.class) + ".equals(colorAsRGB, other.colorAsRGB))");
+        sc.add("return false;");
+        sc.add("if (italic != other.italic)");
+        sc.add("return false;");
+        sc.add("if (strikethrough != other.strikethrough)");
+        sc.add("return false;");
+        sc.add("if (underline != other.underline)");
+        sc.add("return false;");
+        sc.add("return true;");
         sc.add("}");
         sc.addLineBreak();
 	}
