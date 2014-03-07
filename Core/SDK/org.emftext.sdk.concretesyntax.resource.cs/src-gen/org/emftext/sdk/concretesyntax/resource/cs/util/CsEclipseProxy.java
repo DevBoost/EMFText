9955aa131c3015e77cbde2aa72fe71e68de9cac5
Mirko Seifert <mirko.seifert@devboost.de>
2014-03-07 15:15:58 +0100

    Regenerated CS with current SDK Removed unused dependency to EMF validation framework

--- Core/SDK/org.emftext.sdk.concretesyntax.resource.cs/src-gen/org/emftext/sdk/concretesyntax/resource/cs/util/CsEclipseProxy.java
+++ Core/SDK/org.emftext.sdk.concretesyntax.resource.cs/src-gen/org/emftext/sdk/concretesyntax/resource/cs/util/CsEclipseProxy.java
@@ -16,12 +16,8 @@
 
 package org.emftext.sdk.concretesyntax.resource.cs.util;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
@@ -30,23 +26,13 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.InternalEObject;
-import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.Resource.Factory;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.validation.model.ConstraintStatus;
-import org.eclipse.emf.validation.model.EvaluationMode;
-import org.eclipse.emf.validation.service.IBatchValidator;
-import org.eclipse.emf.validation.service.ILiveValidator;
-import org.eclipse.emf.validation.service.ModelValidationService;
 
 /**
  * A utility class that bundles all dependencies to the Eclipse platform. Clients
@@ -153,95 +139,6 @@ public class CsEclipseProxy {
 		}
 		Path path = new Path(platformString);
 		return workspaceRoot.getFile(path);
-	}
-	
-	/**
-	 * Checks all registered EMF validation constraints. Note: EMF validation does not
-	 * work if OSGi is not running.
-	 */
-	@SuppressWarnings("restriction")
-	public void checkEMFValidationConstraints(org.emftext.sdk.concretesyntax.resource.cs.ICsTextResource resource, EObject root, boolean includeBatchConstraints) {
-		// The EMF validation framework code throws a NPE if the validation plug-in is not
-		// loaded. This is a bug, which is fixed in the Helios release. Nonetheless, we
-		// need to catch the exception here.
-		org.emftext.sdk.concretesyntax.resource.cs.util.CsRuntimeUtil runtimeUtil = new org.emftext.sdk.concretesyntax.resource.cs.util.CsRuntimeUtil();
-		if (runtimeUtil.isEclipsePlatformRunning() && runtimeUtil.isEMFValidationAvailable()) {
-			// The EMF validation framework code throws a NPE if the validation plug-in is not
-			// loaded. This is a workaround for bug 322079.
-			if (org.eclipse.emf.validation.internal.EMFModelValidationPlugin.getPlugin() != null) {
-				try {
-					ModelValidationService service = ModelValidationService.getInstance();
-					IStatus status;
-					// Batch constraints are only evaluated if requested (e.g., when a resource is
-					// loaded for the first time).
-					if (includeBatchConstraints) {
-						IBatchValidator validator = service.<EObject, IBatchValidator>newValidator(EvaluationMode.BATCH);
-						validator.setIncludeLiveConstraints(false);
-						status = validator.validate(root);
-						addStatus(status, resource, root, org.emftext.sdk.concretesyntax.resource.cs.CsEProblemType.BATCH_CONSTRAINT_PROBLEM);
-					}
-					// Live constraints are always evaluated
-					ILiveValidator validator = service.<Notification, ILiveValidator>newValidator(EvaluationMode.LIVE);
-					Collection<Notification> notifications = createNotifications(root);
-					status = validator.validate(notifications);
-					addStatus(status, resource, root, org.emftext.sdk.concretesyntax.resource.cs.CsEProblemType.LIVE_CONSTRAINT_PROBLEM);
-				} catch (Throwable t) {
-					new org.emftext.sdk.concretesyntax.resource.cs.util.CsRuntimeUtil().logError("Exception while checking contraints provided by EMF validator classes.", t);
-				}
-			}
-		}
-	}
-	
-	private Collection<Notification> createNotifications(EObject eObject) {
-		List<Notification> notifications = new ArrayList<Notification>();
-		createNotification(eObject, notifications);
-		Iterator<EObject> allContents = eObject.eAllContents();
-		while (allContents.hasNext()) {
-			EObject next = (EObject) allContents.next();
-			createNotification(next, notifications);
-		}
-		return notifications;
-	}
-	
-	private void createNotification(EObject eObject, List<Notification> notifications) {
-		if (eObject instanceof InternalEObject) {
-			InternalEObject internalEObject = (InternalEObject) eObject;
-			Notification notification = new ENotificationImpl(internalEObject, 0, ENotificationImpl.NO_FEATURE_ID, null, null);
-			notifications.add(notification);
-		}
-	}
-	
-	public void addStatus(IStatus status, org.emftext.sdk.concretesyntax.resource.cs.ICsTextResource resource, EObject root, org.emftext.sdk.concretesyntax.resource.cs.CsEProblemType problemType) {
-		List<EObject> causes = new ArrayList<EObject>();
-		causes.add(root);
-		if (status instanceof ConstraintStatus) {
-			ConstraintStatus constraintStatus = (ConstraintStatus) status;
-			Set<EObject> resultLocus = constraintStatus.getResultLocus();
-			causes.clear();
-			causes.addAll(resultLocus);
-		}
-		IStatus[] children = status.getChildren();
-		boolean hasChildren = children != null && children.length > 0;
-		// Ignore composite status objects that have children. The actual status
-		// information is then contained in the child objects.
-		if (!status.isMultiStatus() || !hasChildren) {
-			int severity = status.getSeverity();
-			if (severity == IStatus.ERROR) {
-				for (EObject cause : causes) {
-					resource.addError(status.getMessage(), problemType, cause);
-				}
-			}
-			if (severity == IStatus.WARNING) {
-				for (EObject cause : causes) {
-					resource.addWarning(status.getMessage(), problemType, cause);
-				}
-			}
-		}
-		if (children != null) {
-			for (IStatus child : children) {
-				addStatus(child, resource, root, problemType);
-			}
-		}
 	}
 	
 	/**
