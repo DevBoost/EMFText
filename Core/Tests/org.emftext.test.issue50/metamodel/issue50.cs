d79700af8debbd0010aee49a6c444f613b40591e
Mirko Seifert <mirko.seifert@devboost.de>
2014-04-30 21:34:09 +0200

    Added test for GitHub issue #50

---
+++ Core/Tests/org.emftext.test.issue50/metamodel/issue50.cs
@@ -0,0 +1,12 @@
+SYNTAXDEF issue50
+FOR <http://www.emftext.org/test/issue50>
+START ResultType, ContextType
+
+OPTIONS {
+	licenceHeader ="../../org.dropsbox/licence.txt";
+}
+
+RULES {
+	ResultType ::= "ResultType";
+	ContextType ::= "ContextType";
+}
