943f734aaad53766fac255f41956637c07b326ae
Mirko Seifert <mirko.seifert@devboost.de>
2014-05-08 10:28:08 +0200

    Applied minor refactorings

--- Core/SDK/org.emftext.sdk.codegen.resource/src/org/emftext/sdk/codegen/resource/creators/ANTLRParserCreator.java
+++ Core/SDK/org.emftext.sdk.codegen.resource/src/org/emftext/sdk/codegen/resource/creators/ANTLRParserCreator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2012
+ * Copyright (c) 2006-2014
  * Software Technology Group, Dresden University of Technology
  * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
  * 
@@ -18,22 +18,29 @@ package org.emftext.sdk.codegen.resource.creators;
 import java.io.File;
 
 import org.antlr.Tool;
+import org.antlr.tool.ANTLRErrorListener;
 import org.antlr.tool.ErrorManager;
 import org.emftext.sdk.IPluginDescriptor;
 import org.emftext.sdk.codegen.IArtifactCreator;
 import org.emftext.sdk.codegen.resource.GenerationContext;
 
 /**
- * Runs ANTLR on a .g file to generate Java classes for the parser and
- * the lexer from the ANTLR grammar specification.
+ * Runs ANTLR on a .g file to generate Java classes for the parser and the lexer
+ * from the ANTLR grammar specification.
  */
 public class ANTLRParserCreator implements IArtifactCreator<GenerationContext> {
 
 	public void createArtifacts(IPluginDescriptor plugin, GenerationContext context) {
 		if (context.getANTLRGrammarHasChanged()) {
 			File antlrFile = context.getANTLRGrammarFile(context.getResourcePlugin());
-        	ErrorManager.setErrorListener(new TextResourceGeneratorANTLRErrorListener(context));
-        	Tool antlrTool = new Tool(new String[]{"-Xconversiontimeout", "10000", "-o", antlrFile.getParentFile().getAbsolutePath(), antlrFile.getAbsolutePath()});
+        	
+			ANTLRErrorListener listener = new TextResourceGeneratorANTLRErrorListener(context);
+			ErrorManager.setErrorListener(listener);
+			
+			String[] arguments = new String[] { "-Xconversiontimeout", "10000",
+					"-o", antlrFile.getParentFile().getAbsolutePath(),
+					antlrFile.getAbsolutePath() };
+			Tool antlrTool = new Tool(arguments);
         	antlrTool.process();
 		}
 	}
