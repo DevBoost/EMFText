20b1bb580410ed5f54fe0366b9ea333000226da4
Jan Reimann <jan.reimann@devboost.de>
2016-04-08 12:52:55 +0200

    adjusted license header

--- Core/SDK/org.emftext.sdk.codegen.resource.ui/src/org/emftext/sdk/codegen/resource/ui/generators/ui/DamagerRepairerGenerator.java
+++ Core/SDK/org.emftext.sdk.codegen.resource.ui/src/org/emftext/sdk/codegen/resource/ui/generators/ui/DamagerRepairerGenerator.java
@@ -1,15 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2012-2016 DevBoost GmbH, contact@devboost.de
- *
+ * Copyright (c) 2012-2016
  * DevBoost GmbH, Dresden, Amtsgericht Dresden, HRB 34001
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
- *   DevBoost GmbH - Berlin, Germany
+ *   DevBoost GmbH - Dresden, Germany
  *      - initial API and implementation
  ******************************************************************************/
 package org.emftext.sdk.codegen.resource.ui.generators.ui;
